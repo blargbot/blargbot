ca285633a52c3725c5f4232a1205378223690018
ratismal <kirkbenm@gmail.com>
2016-08-25 11:08:48 -0600

    im blue

--- dcommands/stats.js
+++ dcommands/stats.js
@@ -15,13 +15,13 @@ e.info = 'Gives you some information about me';
 e.category = bu.CommandType.GENERAL
 
 e.execute = (msg, words, text) => {
-    bu.sendMessageToDiscord(msg.channel.id, `\`\`\`diff
+    bu.sendMessageToDiscord(msg.channel.id, `\`\`\`xl
 !== { Stats} ==!
-+ running on ${bot.guilds.size} guilds.
-+ running on ${Object.keys(bot.channelGuildMap).length} channels.
-+ serving ${bot.users.size} users.
-+ using ${bu.getMemoryUsage()}MiB RAM
-+ running for ${bu.createTimeDiffString(moment(), bu.startTime)}
-- current version: ${bu.VERSION}
+Running on ${bot.guilds.size} guilds.
+Running on ${Object.keys(bot.channelGuildMap).length} channels.
+Serving ${bot.users.size} users.
+Using ${bu.getMemoryUsage()}MiB RAM
+Running for ${bu.createTimeDiffString(moment(), bu.startTime)}
+Version: ${bu.VERSION}
 \`\`\``);
 }
