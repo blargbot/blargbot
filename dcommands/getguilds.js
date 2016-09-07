b5cb13b7836397846000d5c766b0ffa87cc41057
ratismal <kirkbenm@gmail.com>
2016-09-07 16:08:32 -0600

    light filtering

--- dcommands/getguilds.js
+++ dcommands/getguilds.js
@@ -15,11 +15,17 @@ e.category = blargutil.CommandType.CAT
 
 e.execute = (msg, words, text) => {
     if (msg.author.id === blargutil.CAT_ID) {
+        var gArray = bot.guilds.map(m => m)
+        if (words[1]) {
+            if (words[1].toLowerCase() == 'bots') {
+                gArray = bot.guilds.filter(m => m.name.toLowerCase().indexOf('bot') > -1)
+            }
+        }
         var messages = []
         var i = 0;
         messages.push(`Guilds (page ${i}):\n`)
-        bot.guilds.forEach(function (guild, id) {
-            var addTo = ` - ${guild.name} (${id})\n`;
+        gArray.forEach(function (guild, id) {
+            var addTo = ` - ${guild.name} (${guild.id})\n`;
             if (messages[i].length + addTo.length > 2000) {
                 i++;
                 messages.push(`Guilds (page ${i}):\n`)
