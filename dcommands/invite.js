cab236bd267e86c06daed8c1c2df19668651d64b
ratismal <kirkbenm@gmail.com>
2016-09-09 23:17:17 -0600

    perms in invite link

--- dcommands/invite.js
+++ dcommands/invite.js
@@ -15,6 +15,9 @@ e.alias = ['join'];
 e.category = bu.CommandType.GENERAL;
 
 e.execute = (msg) => {
-    bu.sendMessageToDiscord(msg.channel.id, 'Invite me to your guild!\nhttps://discordapp.com/oauth2/authorize?client_id=170237838334492682&scope=bot\n'
+    bu.sendMessageToDiscord(msg.channel.id, 'Invite me to your guild!\n' 
+    + 'https://discordapp.com/oauth2/authorize?client_id=170237838334492682&scope=bot&permissions=268495894\n'
+    + 'Don\'t need the moderation functions? Use this link instead:'
+    + 'https://discordapp.com/oauth2/authorize?client_id=170237838334492682&scope=bot\n'
         + 'Join my guild!\nhttps://discord.gg/015GVxZxI8rtlJgXF');
 };
