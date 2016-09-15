66f76a060c80b1e63605830623c2dd6bdbd81ce1
ratismal <kirkbenm@gmail.com>
2016-09-15 16:49:44 -0600

    bot percentage

--- dcommands/getguilds.js
+++ dcommands/getguilds.js
@@ -16,19 +16,23 @@ e.category = blargutil.CommandType.CAT;
 e.execute = (msg, words) => {
     if (msg.author.id === blargutil.CAT_ID) {
         var gArray;
+        var botRatio = false;
         if (words[1]) {
             if (words[1].toLowerCase() == 'bots') {
                 gArray = bot.guilds.filter(m => m.members.filter(m2 => m2.user.bot).length / m.memberCount > 0.8 || /bot/gi.test(m.name));
+                botRatio = true;
             }
         }
         if (!gArray) {
-        var gArray = bot.guilds.map(m => m);            
+            gArray = bot.guilds.map(m => m);
         }
         var messages = [];
         var i = 0;
         messages.push(`Guilds (page ${i}):\n`);
         gArray.forEach(function (guild) {
-            var addTo = ` - ${guild.name} (${guild.id})\n`;
+            var addTo = ` - ${guild.name} (${guild.id})${botRatio
+                ? ` ${Math.round(guild.members.filter(m2 => m2.user.bot).length / guild.memberCount * 100)}% Bots`
+                : ''}\n`;
             if (messages[i].length + addTo.length > 2000) {
                 i++;
                 messages.push(`Guilds (page ${i}):\n`);
