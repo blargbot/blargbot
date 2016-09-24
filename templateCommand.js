7f4268052982aea2603a4c4ef325c4a1e00b9754
ratismal <kirkbenm@gmail.com>
2016-09-23 18:33:17 -0600

    new template

--- templateCommand.js
+++ templateCommand.js
@@ -5,6 +5,7 @@ var bot;
 e.init = (Tbot, blargutil) => {
     bot = Tbot;
     bu = blargutil;
+    e.category = bu.CommandType.GENERAL;
 };
 
 e.requireCtx = require;
@@ -13,7 +14,6 @@ e.isCommand = true;
 e.hidden = false;
 e.usage = '';
 e.info = '';
-e.category = bu.CommandType.GENERAL;
 
 e.execute = (msg, words, text) => {
 
