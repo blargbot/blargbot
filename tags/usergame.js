7d38f7ea49e5c812155bf7aa123c35457a1bb825
ratismal <kirkbenm@gmail.com>
2016-09-27 20:00:04 -0600

    fixed usergame having wrong examples

--- tags/usergame.js
+++ tags/usergame.js
@@ -18,8 +18,8 @@ e.usage = `{usergame[;user[;quiet]]}`;
 e.desc = `Returns the game the user is playing. If the user isn't playing a game, returns the word 'nothing'. If <code>name</code> is specified, gets that user instead. If
                                 <code>quiet</code> is
                                 specified, if a user can't be found it will simply return the <code>name</code>`;
-e.exampleIn = `You are playing is {useravatar}`;
-e.exampleOut = `Your avatar is (avatar url)`;
+e.exampleIn = `You are playing {game}`;
+e.exampleOut = `You are playing with bbtag`;
 
 
 e.execute = (msg, args, fallback) => {
