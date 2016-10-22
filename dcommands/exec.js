8989ccebba8bee1412a5f97b06e464d1d45e20b8
ratismal <kirkbenm@gmail.com>
2016-10-21 20:38:13 -0600

    ffs

--- dcommands/exec.js
+++ dcommands/exec.js
@@ -22,9 +22,10 @@ e.execute = (msg, words, text) => {
         var commandToProcess = words.slice(1).join(' ');
         bu.logger.debug(commandToProcess);
         if (commandToProcess.trim().toLowerCase() == 'pm2 restart 0'
-        || commandToProcess.trim().toLowerCase() == 'pm2 reload 0'
-        || commandToProcess.trim().toLowerCase() == 'pm2 start 0') {
+            || commandToProcess.trim().toLowerCase() == 'pm2 reload 0'
+            || commandToProcess.trim().toLowerCase() == 'pm2 start 0') {
             bu.send(msg.channel.id, 'No! That\'s dangerous! Do `b!restart` instead.\n\nIt\'s not that I don\'t trust you, it\'s just...\n\nI don\'t trust you.');
+            return;
         }
         exec(commandToProcess, function (err, stdout, stderr) {
             if (err) {
