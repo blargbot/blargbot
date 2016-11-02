55364950b6a3b6423d97f942a0122f7c2d0dd89b
ratismal <kirkbenm@gmail.com>
2016-11-01 20:08:13 -0600

    made announce public

--- dcommands/announce.js
+++ dcommands/announce.js
@@ -6,7 +6,7 @@ e.init = () => {
 
 
 
-    e.category = bu.CommandType.CAT;
+    e.category = bu.CommandType.ADMIN;
 
 };
 
@@ -14,13 +14,45 @@ e.requireCtx = require;
 
 e.isCommand = true;
 e.hidden = false;
-e.usage = 'announce <stuff>';
+e.usage = 'announce <text>';
 e.info = 'Makes an annoucement';
 var changeChannel = '229135592720433152';
 var roleId = '239396821645000704';
 
 e.execute = async function (msg, words) {
-    if (msg.author.id != bu.CAT_ID) {
+    var changeChannel, roleId;
+    let storedGuild = await bu.r.table('guild').get(msg.channel.guild.id).run();
+    if (storedGuild.hasOwnProperty('announce')) {
+        changeChannel = storedGuild.announce.channel;
+        roleId = storedGuild.announce.role;
+    } else {
+        let msg2 = await bu.awaitMessage(msg,
+            'This guild doesn\'t have announcements set up. Please mention the channel that announcements should be put in.',
+            m => {
+                if (m.channelMentions.length > 0) return true;
+                else return false;
+            });
+        changeChannel = msg2.channelMentions[0];
+        msg2 = await bu.awaitMessage(msg,
+            'Please type the name or ID of the announcements role.');
+        let role = await bu.getRole(msg, msg2.content);
+        if (role != null) {
+            roleId = role.id;
+        } else {
+            bu.send(msg, `I couldn't find a role with that name. Run \`b!announce\` to attempt the setup again.`);
+            return;
+        }
+        await bu.r.table('guild').get(msg.channel.guild.id).update({
+            announce: {
+                channel: changeChannel,
+                role: roleId
+            }
+        }).run();
+    }
+    if (msg.channel.guild.channels.get(changeChannel) == undefined || msg.channel.guild.roles.get(roleId) == undefined) {
+        bu.send(msg, `The assigned channel or role has been deleted, the config will be cleaned. Please run the command again.`);
+        delete storedGuild.announce;
+        await bu.r.table('guild').get(msg.channel.guild.id).replace(storedGuild).run();
         return;
     }
     words.shift();
@@ -31,19 +63,21 @@ e.execute = async function (msg, words) {
 **${msg.author.username}#${msg.author.discriminator}** has made the following announcement:
 
 ${message}`;
-logger.debug(message);
+    logger.debug(message);
     try {
         await role.edit({
             mentionable: true
         });
-        logger.debug('editted role to mentionable');
-        await bu.send(changeChannel, message);
-        logger.debug('sent message');
-        await role.edit({
-            mentionable: false
-        });
-        logger.debug('edited role to unmentionable');
     } catch (err) {
         logger.error(err);
     }
+    await bu.send(changeChannel, message);
+    try {
+        await role.edit({
+            mentionable: false
+        });
+    } catch (err) {
+        logger.error(err);
+    }
+
 };
