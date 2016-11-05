92f2f5ef33d835b090633ee9252864552edf6926
ratismal <kirkbenm@gmail.com>
2016-11-05 15:22:56 -0600

    AAAAAA

--- discord.js
+++ discord.js
@@ -889,7 +889,7 @@ ${newMsg}`);
                 if (oldUser.username != user.username) username = '**Old Name:** ' + oldUser.username + '\n';
                 if (oldUser.discriminator != user.discriminator) discrim = '**Old Discriminator:** ' + oldUser.discriminator + '\n';
                 let message = `**User:** ${user.username}#${user.discriminator} (${user.id})${username || ''}
-${discrim || ''}${user.avatar != oldUser.avatar ? '**New Avatar:** <' + user.avatarURL + '>\n**Old Avatar:** <https://cdn.discordapp.com/avatars/' + 'user.id' + '/' + oldUser.avatar + '.jpg>' : ''}
+${discrim || ''}${user.avatar != oldUser.avatar ? '**New Avatar:** <' + user.avatarURL + '>\n**Old Avatar:** <https://cdn.discordapp.com/avatars/' + user.id + '/' + oldUser.avatar + '.jpg>' : ''}
 `;
                 guilds.forEach(g => {
                     bu.logEvent(g.id, 'userupdate', message);
