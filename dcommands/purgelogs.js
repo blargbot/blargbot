786860af3747dff87c215b6ec1397f4212556f26
ratismal <kirkbenm@gmail.com>
2016-11-05 13:26:53 -0600

    changed to two weeks

--- dcommands/purgelogs.js
+++ dcommands/purgelogs.js
@@ -10,19 +10,19 @@ e.hidden = false;
 e.usage = '';
 e.info = '';
 
-e.execute = async function (msg) {
+e.execute = async function(msg) {
     if (msg.author.id === bu.CAT_ID) {
-        await bu.send(msg, 'Ok, I\'ll purge all chat log records over 7 days old. This is going to take a while, so I\'ll ping you once I\'m done.');
+        await bu.send(msg, 'Ok, I\'ll purge all chat log records that are over two weeks old. This is going to take a while, so I\'ll ping you once I\'m done.');
         let start = moment();
         let returnObj = await r.table('chatlogs')
-            .between(r.epochTime(0), r.now().sub(7 * 24 * 60 * 60), {
+            .between(r.epochTime(0), r.now().sub(14 * 24 * 60 * 60), {
                 index: 'msgtime'
             }).delete({
                 durability: "soft"
             }).run();
-            let end = moment();
-            let diff = moment.duration(end - start);
-            bu.send(msg, `Ok, ${msg.author.mention}! I'm finished!
+        let end = moment();
+        let diff = moment.duration(end - start);
+        bu.send(msg, `Ok, ${msg.author.mention}! I'm finished!
 ${returnObj.deleted} records were deleted.
 The operation took:
     ${diff.days()} days
