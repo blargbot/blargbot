14e1728c7330a7a744a6608370e890f74b429881
ratismal <kirkbenm@gmail.com>
2016-11-16 10:28:44 -0700

    Can blacklist any channel from any channel

--- dcommands/blacklist.js
+++ dcommands/blacklist.js
@@ -13,26 +13,28 @@ e.init = () => {
 e.isCommand = true;
 
 e.hidden = false;
-e.usage = 'blacklist';
-e.info = 'Blacklists the current channel. The bot will not respond until you do `blacklist` again.';
+e.usage = 'blacklist [channel]';
+e.info = 'Blacklists the current channel, or the first channel that you mention. The bot will not respond until you do `blacklist` again.';
 e.longinfo = `<p>Blacklists the current channel. The bot will not respond until you do the command again.</p>`;
 
 e.execute = async function(msg) {
-
+    let channelid = msg.channel.id;
+    if (msg.channelMentions.length > 0) channelid = msg.channelMentions[0];
 
     let storedGuild = await bu.getGuild(msg.guild.id);
-    let channel = storedGuild.channels && storedGuild.channels.hasOwnProperty(msg.channel.id) ?
-        storedGuild.channels[msg.channel.id] : {
+    let channel = storedGuild.channels && storedGuild.channels.hasOwnProperty(channelid) ?
+        storedGuild.channels[channelid] : {
             nsfw: false
         };
+
     if (channel.blacklisted) {
         channel.blacklisted = false;
-        bu.send(msg, 'This channel is no longer blacklisted.');
+        bu.send(msg, '**' + bot.getChannel(channelid).name + '** is no longer blacklisted.');
     } else {
         channel.blacklisted = true;
-        bu.send(msg, 'This channel is now blacklisted.');
-
+        bu.send(msg, '**' + bot.getChannel(channelid).name + '** is now blacklisted.');
     }
+
     storedGuild.channels[msg.channel.id] = channel;
     r.table('guild').get(msg.channel.guild.id).update({
         channels: storedGuild.channels
