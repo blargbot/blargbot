52e8445c2e80714c083ccd357f83f06ca5e94c06
ratismal <kirkbenm@gmail.com>
2016-12-03 09:30:39 -0700

    whoops moment

--- backend/routes/tags.js
+++ backend/routes/tags.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 const hbs = require('hbs');
-
+const moment = require('moment');
 
 router.get('/', (req, res) => {
     res.locals.user = req.user;
