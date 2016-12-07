4b52205e3568c6627dc0a53d9df912395665f71f
ratismal <kirkbenm@gmail.com>
2016-12-07 13:19:50 -0700

    updated patrons

--- dcommands/info.js
+++ dcommands/info.js
@@ -15,7 +15,17 @@ e.usage = 'info';
 e.info = 'Returns some info about me.';
 e.longinfo = `<p>Gets information about the specified user.</p>`;
 
+const patrons = [
+    'Nex',
+    '228574821590499329'
+];
+
 e.execute = (msg) => {
+    let patronStr = patrons.map(p => {
+        if (/^[0-9]{17,23}$/.test(p)) {
+            return bu.getFullName(bot.users.get(p));
+        } else return p;
+    }).join('\n - ');
     try {
         bu.send(msg, `blargbot is a multipurpose bot with new features implemented regularly, written in javascript using Eris.
 https://blargbot.xyz
@@ -29,9 +39,10 @@ ${pad('Uptime:', 10)} ${bu.createTimeDiffString(moment(), bu.startTime)}
 ${pad('Version:', 10)} ${bu.VERSION}
 \`\`\`
 
-Special thanks to my patron, Nex! :thumbsup:
+:heart: __**Special thanks to my patrons!**__ :heart:
+** - ${patronStr}**
 
-Additional credits to Aurieh#0258! :heart:
+Additional credits to Aurieh#0258! :thumbsup:
 
 For commands, do \`help\`. For information about supporting me, do \`donate\``);
     } catch (err) {
