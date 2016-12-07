732d47fec852ee4d0251eb876b7858074bf9f479
ratismal <kirkbenm@gmail.com>
2016-12-07 15:08:49 -0700

    yes, more

--- dcommands/catbot.js
+++ dcommands/catbot.js
@@ -15,58 +15,66 @@ e.usage = '';
 e.info = '';
 
 e.execute = async function(msg, words) {
-    await bot.sendChannelTyping(msg.channel.id);
-    if (words[1]) {
-        switch (words[1].toLowerCase()) {
-            case 'chis':
-                genlogs(msg, '100463282099326976', 'chis');
-                break;
-            case 'mindy':
-                genlogs(msg, '217122202934444033', 'mindy');
-                break;
-            case 'zeta':
-                genlogs(msg, '94129005791281152', 'zeta');
-                break;
-            case 'fuyu':
-                genlogs(msg, ['141545699442425856', '214796473689178133'], 'fuyu');
-                break;
-            case 'triping':
-                genlogs(msg, '128694295170514944', 'triping');
-                break;
-            case 'hk':
-                genlogs(msg, '104360151208706048', 'hk');
-                break;
-            case 'tttie':
-                genlogs(msg, '150628341316059136', 'tttie');
-                break;
-            case 'blarg':
-                genlogs(msg, '134133271750639616', 'blarg');
-                break;
-            case 'xeta':
-                genlogs(msg, '155490847494897664', 'xeta');
-                break;
-            case 'axonium':
-                genlogs(msg, '133332642685779968', 'axonium');
-                break;
-            case 'notso':
-                genlogs(msg, '130070621034905600', 'notso');
-                break;
-            case 'mine':
-                genlogs(msg, '155112606661607425', 'mine');
-                break;
+    if (msg.author.id == bu.CAT_ID) {
+        await bot.sendChannelTyping(msg.channel.id);
+        if (words[1]) {
+            switch (words[1].toLowerCase()) {
+                case 'chis':
+                    genlogs(msg, '100463282099326976', 'chis');
+                    break;
+                case 'mindy':
+                    genlogs(msg, '217122202934444033', 'mindy');
+                    break;
+                case 'zeta':
+                    genlogs(msg, '94129005791281152', 'zeta');
+                    break;
+                case 'fuyu':
+                    genlogs(msg, ['141545699442425856', '214796473689178133'], 'fuyu');
+                    break;
+                case 'triping':
+                    genlogs(msg, '128694295170514944', 'triping');
+                    break;
+                case 'hk':
+                    genlogs(msg, '104360151208706048', 'hk');
+                    break;
+                case 'tttie':
+                    genlogs(msg, '150628341316059136', 'tttie');
+                    break;
+                case 'blarg':
+                    genlogs(msg, '134133271750639616', 'blarg');
+                    break;
+                case 'xeta':
+                    genlogs(msg, '155490847494897664', 'xeta');
+                    break;
+                case 'axonium':
+                    genlogs(msg, '133332642685779968', 'axonium');
+                    break;
+                case 'notso':
+                    genlogs(msg, '130070621034905600', 'notso');
+                    break;
+                case 'mine':
+                    genlogs(msg, '155112606661607425', 'mine');
+                    break;
+                case 'abal':
+                    genlogs(msg, ['150061853001777154', '98295630480314368'], 'abal');
+                    break;
+                case 'pollr':
+                    genlogs(msg, '168169809303961600', 'pollr');
+                    break;
+            }
+        } else {
+            let catMsgs = await r.table('catchat').orderBy('msgid');
+            let content = [];
+            for (let message of catMsgs) {
+                if (message.guildid != '197529405659021322' && message.nsfw == 0)
+                    content.push(message.content);
+            }
+            fs.writeFile(path.join(__dirname, '..', '..', 'catbot', 'cat.json'),
+                JSON.stringify(content, null, 2), (err) => {
+                    if (err) bu.send(msg, err);
+                    bu.send(msg, 'Done!');
+                });
         }
-    } else {
-        let catMsgs = await r.table('catchat').orderBy('msgid');
-        let content = [];
-        for (let message of catMsgs) {
-            if (message.guildid != '197529405659021322' && message.nsfw == 0)
-                content.push(message.content);
-        }
-        fs.writeFile(path.join(__dirname, '..', '..', 'catbot', 'cat.json'),
-            JSON.stringify(content, null, 2), (err) => {
-                if (err) bu.send(msg, err);
-                bu.send(msg, 'Done!');
-            });
     }
 };
 
