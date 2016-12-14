0f6775133dfd6a4023abaa49098a40a19b4cb4d8
ratismal <kirkbenm@gmail.com>
2016-12-13 20:50:48 -0700

    newline tag

---
+++ tags/newline.js
@@ -0,0 +1,47 @@
+var e = module.exports = {};
+
+e.init = () => {
+    e.category = bu.TagType.COMPLEX;
+};
+
+e.requireCtx = require;
+
+e.isTag = true;
+e.name = `newline`;
+e.args = `[length]`;
+e.usage = `{newline[;length]}`;
+e.desc = `Will be replaced by a specified number of newline characters (\\n).`;
+e.exampleIn = `{newline}Hello, world!`;
+e.exampleOut = `\nHello, world!`;
+
+
+e.execute = async function(params) {
+    let length = 1;
+    var parsedFallback = parseInt(params.fallback);
+    if (params.args[1]) {
+        length = parseInt(await bu.processTagInner(params, 1));
+        if (isNaN(length)) {
+            if (isNaN(parsedFallback)) {
+                return {
+                    replaceString: await bu.tagProcessError(params, params.fallback, '`Not a number`'),
+                    replaceContent: replaceContent
+                };
+            } else {
+                length = parsedFallback;
+            }
+        }
+    }
+    var replaceString = '';
+
+    for (let i = 0; i < length; i++) {
+        replaceString += '\n';
+    }
+
+    var replaceContent = false;
+
+
+    return {
+        replaceString: replaceString,
+        replaceContent: replaceContent
+    };
+};
