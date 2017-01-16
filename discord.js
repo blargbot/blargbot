5fd758a70787e43557661468d20ed83d6a5d6f43
stupid cat <kirkbenm@gmail.com>
2017-01-16 14:44:13 -0700

    abal please

--- discord.js
+++ discord.js
@@ -772,7 +772,7 @@ function registerListeners() {
     });
 
     bot.on('error', function(err, id) {
-        if (error)
+        if (error && err.message.indexOf('Message.guild') == -1)
             logger.error(`[${id}] ${err.stack}`);
     });
 
