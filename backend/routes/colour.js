9fdd3675053eb4062ed6bf73b1a26a044589b54c
stupid cat <kirkbenm@gmail.com>
2017-02-03 16:58:09 -0700

    fixed color

--- backend/routes/colour.js
+++ backend/routes/colour.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const Jimp = require('jimp');
 
 router.get('/:hex', async function(req, res) {
-    while (req.params.hex.length > 6) req.params.hex = req.params.hex.substring(0, 6);
-    let hex = Number('0x' + req.params.hex);
+    if (req.params.hex.length > 6) req.params.hex = req.params.hex.substring(0, 6);
+    let hex = Number('0xff' + req.params.hex);
     if (isNaN(hex)) {
         res.status(400).json({
             error: 'That is not a hex code, you scrub!'
@@ -12,16 +12,16 @@ router.get('/:hex', async function(req, res) {
         return;
     }
     let img = new Jimp(128, 128, hex);
-    img.toBuffer(Jimp.MIME_PNG, (err, buff) => {
+    img.getBuffer(Jimp.MIME_JPEG, (err, buf) => {
         logger.website(req.params.hex, hex, buf.length);
 
         res.writeHead(200, {
-            'Content-Type': 'image/png',
+            'Content-Type': 'image/jpeg',
             'Content-Length': buf.length
         });
         res.end(buf, 'binary');
 
-    })
+    });
 });
 
 module.exports = router;
