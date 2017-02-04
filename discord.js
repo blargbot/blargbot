72ff84c30df4462dc5b98c19c32d814a705e0b97
stupid cat <kirkbenm@gmail.com>
2017-02-04 10:01:27 -0700

    No, that's not right

--- discord.js
+++ discord.js
@@ -1010,7 +1010,7 @@ If you are the owner of this server, here are a few things to know.
 
     bot.on('messageUpdate', async function(msg, oldmsg) {
         if (!msg.author) {
-            logger.info(msg.id, msg.channel.id, msg.embed);
+            logger.info(msg.id, msg.channel.id, util.inspect(msg.embed));
         }
         if (msg.author) {
             if (!msg.guild) return;
