f8f937b0c77895b9065fe079a6e8891a92e8f73f
stupid cat <kirkbenm@gmail.com>
2017-02-27 13:24:40 -0700

    brb need to get config

--- index.js
+++ index.js
@@ -3,3 +3,8 @@
  * made by stupid cat and stuff
  * yeah
  */
+
+const Core = require('./Core');
+
+global.config = require('./Data/config.json');
+config.avatars = require(config.general.isbeta ? './Data/avatarsBeta.json' : './Data/avatars.json');
