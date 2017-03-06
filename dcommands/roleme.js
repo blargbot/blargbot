0f4f3f78e9de36dd147ebe2b083dc62efbebc22c
stupid cat <kirkbenm@gmail.com>
2017-03-05 20:11:37 -0700

    add edit to docs

--- dcommands/roleme.js
+++ dcommands/roleme.js
@@ -8,7 +8,7 @@ e.requireCtx = require;
 
 e.isCommand = true;
 e.hidden = false;
-e.usage = 'roleme <list | add | remove>';
+e.usage = 'roleme <list | add | remove | edit>';
 e.info = 'A roleme is a system to automatically give/remove roles to a user when they say a specific catchphrase. You can make these catchphrases anything you want, case sensitive/insensitive, and only activate in specific channels. The roleme command has three subcommands:\n\n**list**: lists all the rolemes active on the guild.\n**add**: adds a roleme to the guild. Just follow the instructions, or use flags.\n**remove**: returns a list of rolemes so you can choose one to remove.\n**edit**: modifies a roleme using the provided flags';
 e.long = `<p>A roleme is a system to automatically give/remove roles to a 
 user when they say a specific catchphrase. You can make these catchphrases 
@@ -20,38 +20,38 @@ specific channels. The roleme command has three subcommands:</p>
 <li>edit: modifies a roleme using the provided flags</li></ul>`;
 
 e.flags = [{
-        flag: 'a',
-        word: 'add',
-        desc: 'Add: A list of roles to add in the roleme'
-    },
-    {
-        flag: 'r',
-        word: 'remove',
-        desc: 'Add: A list of roles to remove in the roleme'
-    },
-    {
-        flag: 'p',
-        word: 'phrase',
-        desc: 'Add: The phrase to respond to'
-    },
-    {
-        flag: 'C',
-        word: 'case',
-        desc: 'Add: Whether the phrase is case sensitive'
-    },
-    {
-        flag: 'c',
-        word: 'channel',
-        desc: 'Add: The channels the roleme should be in'
-    },
-    {
-        flag: 'm',
-        word: 'message',
-        desc: 'Add: The BBTag-compatible message to output on activation'
-    }
+    flag: 'a',
+    word: 'add',
+    desc: 'Add: A list of roles to add in the roleme'
+},
+{
+    flag: 'r',
+    word: 'remove',
+    desc: 'Add: A list of roles to remove in the roleme'
+},
+{
+    flag: 'p',
+    word: 'phrase',
+    desc: 'Add: The phrase to respond to'
+},
+{
+    flag: 'C',
+    word: 'case',
+    desc: 'Add: Whether the phrase is case sensitive'
+},
+{
+    flag: 'c',
+    word: 'channel',
+    desc: 'Add: The channels the roleme should be in'
+},
+{
+    flag: 'm',
+    word: 'message',
+    desc: 'Add: The BBTag-compatible message to output on activation'
+}
 ];
 
-e.execute = async function(msg, words) {
+e.execute = async function (msg, words) {
     let input = bu.parseInput(e.flags, words);
     if (input.undefined[0]) {
         let storedGuild = await bu.getGuild(msg.guild.id);
