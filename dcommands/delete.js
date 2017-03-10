f24cc3807b0ad20e50654a9ef81119ef07f99ed7
stupid cat <kirkbenm@gmail.com>
2017-03-10 12:26:06 -0700

    filter mentions

--- dcommands/delete.js
+++ dcommands/delete.js
@@ -14,7 +14,7 @@ e.longinfo = `<p>Shows that you're about to delete something.</p>`;
 
 e.execute = async function(msg, words) {
     if (words.length > 1) {
-        let input = words.slice(1).join('\n').replace(/\n/gim, ' ').substring(0, 256);
+        let input = bu.filterMentions(words.slice(1).join('\n').replace(/\n/gim, ' ').substring(0, 256));
         let code = bu.genEventCode();
         bot.sendChannelTyping(msg.channel.id);
 
