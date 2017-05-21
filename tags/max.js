70bf67abcf54bbe57376bca301a802a35e587268
stupid cat <kirkbenm@gmail.com>
2017-05-21 13:25:45 -0600

    min and max

---
+++ tags/max.js
@@ -0,0 +1,54 @@
+/*
+ * @Author: stupid cat
+ * @Date: 2017-05-21 13:17:14
+ * @Last Modified by: stupid cat
+ * @Last Modified time: 2017-05-21 13:25:14
+ *
+ * This project uses the AGPLv3 license. Please read the license file before using/adapting any of the code.
+ */
+
+var e = module.exports = {};
+
+e.init = () => {
+    e.category = bu.TagType.COMPLEX;
+};
+
+e.requireCtx = require;
+
+e.array = true;
+e.isTag = true;
+e.name = `max`;
+e.args = `&lt;number&gt;...`;
+e.usage = `{max;number...}`;
+e.desc = `Returns the largest number out of those provided`;
+e.exampleIn = `{min;50;2;65}`;
+e.exampleOut = `65`;
+
+e.execute = async function (params) {
+    for (let i = 1; i < params.args.length; i++) {
+        params.args[i] = await bu.processTagInner(params, i);
+    }
+    let args = params.args,
+        fallback = params.fallback;
+    var replaceString = '';
+    var replaceContent = false;
+
+    let numArray = [];
+    if (params.args[1]) {
+        let deserialized = bu.deserializeTagArray(args[1]);
+
+        if (deserialized && Array.isArray(deserialized.v)) {
+            numArray = deserialized;
+        } else {
+            numArray = params.args.slice(1);
+        }
+        replaceString = Math.max(...numArray);
+    } else {
+        replaceString = await bu.tagProcessError(params, '`Not enough arguments`');
+    }
+    return {
+        terminate: params.terminate,
+        replaceString: replaceString,
+        replaceContent: replaceContent
+    };
+};
