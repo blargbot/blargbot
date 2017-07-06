6c85c8c3fe36a30cc9385b7ff7fae7ff7c34f7ea
stupid cat <kirkbenm@gmail.com>
2017-07-05 22:18:29 -0600

    owo whats this

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,7 +2,7 @@
  * @Author: stupid cat
  * @Date: 2017-05-07 18:19:37
  * @Last Modified by: stupid cat
- * @Last Modified time: 2017-05-07 18:19:37
+ * @Last Modified time: 2017-07-05 22:18:23
  *
  * This project uses the AGPLv3 license. Please read the license file before using/adapting any of the code.
  */
@@ -15,6 +15,13 @@ router.get('/', (req, res) => {
 
     res.render('index');
 });
+
+router.get('/main', (req, res) => {
+    res.locals.user = req.user;
+    req.session.returnTo = req.path;
+
+    res.render('bsod-netneut');
+})
 
 router.get('/editor', (req, res) => {
     res.redirect('/tags/editor');
