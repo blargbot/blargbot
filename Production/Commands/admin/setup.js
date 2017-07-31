347a7f8a0e2c141b80c6e0a0eff60f890eb4a938
stupid cat <kirkbenm@gmail.com>
2017-07-31 12:19:56 -0600

    setup announcements and mute

--- Production/Commands/admin/setup.js
+++ Production/Commands/admin/setup.js
@@ -18,6 +18,11 @@ class ModCommand extends AdminCommand {
             keys: {
                 setstaffrole: `.setstaffrole`,
                 staffrolequery: `.staffrolequery`,
+                mutesetrole: `.mute.setrole`,
+                muterolequery: `.mute.rolequery`,
+                announceset: `.announce.set`,
+                announcerolequery: `announce.rolequery`,
+                announcechannelquery: `announce.channelquery`,
                 nochange: 'generic.nochange'
             }
         });
@@ -28,11 +33,63 @@ class ModCommand extends AdminCommand {
     }
 
     async sub_mute(ctx) {
-        return 'mute';
+        let menu = this.client.Helpers.Menu.build(ctx);
+        let data = ctx.guild.data;
+
+        let roles = ctx.guild.roles.map(r => r).sort((a, b) => {
+            return b.position - a.position;
+        }).map(r => {
+            return {
+                name: `${r.name} (#${r.color.toString(16)})`,
+                value: r.id
+            };
+        });
+        try {
+            menu.embed.setDescription(await ctx.decode(this.keys.muterolequery));
+            let selected = await menu.paginate(roles, false);
+            await data.setKey('mutedRole', selected.value);
+            await ctx.decodeAndSend(this.keys.mutesetrole);
+        } catch (err) {
+            console.error(err);
+            await ctx.decodeAndSend(this.keys.nochange);
+        }
     }
 
     async sub_announcement(ctx) {
-        return 'announcement';
+        let menu = this.client.Helpers.Menu.build(ctx);
+        let data = ctx.guild.data;
+
+        let channels = ctx.guild.channels.map(r => r).sort((a, b) => {
+            return a.position - b.position;
+        }).map(r => {
+            return {
+                name: `<#${r.id}>`,
+                value: r.id
+            };
+        });
+
+        let roles = ctx.guild.roles.map(r => r).sort((a, b) => {
+            return b.position - a.position;
+        }).map(r => {
+            return {
+                name: `${r.name} (#${r.color.toString(16)})`,
+                value: r.id
+            };
+        });
+
+        try {
+            menu.embed.setDescription(await ctx.decode(this.keys.announcechannelquery));
+            let selectedChannel = await menu.paginate(channels, false);
+            menu = this.client.Helpers.Menu.build(ctx);
+            menu.embed.setDescription(await ctx.decode(this.keys.announcerolequery));
+            let selectedRole = await menu.paginate(roles, false);
+            await data.setKey('announcementChannel', selectedChannel.value);
+            await data.setKey('announcementRole', selectedRole.value);
+            await ctx.decodeAndSend(this.keys.announceset);
+        } catch (err) {
+            console.error(err);
+            await ctx.decodeAndSend(this.keys.nochange);
+        }
     }
 
     async sub_staffuser(ctx) {
