4f2f661c41e6986edc1a45c647a7f39b815f1cc8
stupid cat <kirkbenm@gmail.com>
2017-08-01 08:31:32 -0600

    mreow

--- Production/Commands/owner/poe.js
+++ Production/Commands/owner/poe.js
@@ -7,7 +7,8 @@ class EvalCommand extends CatCommand {
         super(client, {
             name: 'poe',
             subcommands: {
-                export: {}
+                import: {},
+                export: { aliases: ['sync'] }
             }
         });
     }
@@ -21,12 +22,33 @@ class EvalCommand extends CatCommand {
             'api_token': _config.api.poeditor.key,
             'id': _config.api.poeditor.id
         };
+        let res = await superagent.get('https://poeditor.com/api/webhooks/github')
+            .query({
+                api_token: args.api_token,
+                id_project: args.id,
+                language: 'en-us',
+                operation: 'sync_terms_and_translations',
+                overwrite_translations: 1,
+                fuzzy_trigger: 1
+            });
+
+        return 'Terms have been exported from GitHub -> POEditor.';
+    }
+
+    async sub_import(ctx) {
+        let args = {
+            'api_token': _config.api.poeditor.key,
+            'id': _config.api.poeditor.id
+        };
         let res = await superagent.post('https://api.poeditor.com/v2/languages/list').type('form').send(args);
         if (res.body.response.status === 'success') {
             let langList = [];
             let failedList = [];
+            let message = 'Importing languages from POEditor -> GitHub...\n\n';
+            let msg2 = await ctx.send('Importing languages from POEditor -> GitHub...');
             for (const lang of res.body.result.languages) {
                 if (lang.percentage >= 60) {
+                    await msg2.edit(message + `Importing \`${lang.code}\` ${lang.name}`);
                     try {
                         let res2 = await superagent.get('https://poeditor.com/api/webhooks/github')
                             .query({
@@ -42,13 +64,13 @@ class EvalCommand extends CatCommand {
                     }
                 }
             }
-            return `Exported the following languages:
-${langList.map(l => ' - ' + l.name + ' (' + l.percentage + '%)').join('\n')}
+            await msg2.edit(message + `Exported the following languages:
+${langList.map(l => ' - `' + l.code + '` ' + l.name + ' (' + l.percentage + '%)').join('\n')}
 
 The following languages failed to export:
-${failedList.map(l => ' - ' + l.name + ' (' + l.percentage + '%)').join('\n')}
+${failedList.map(l => ' - `' + l.code + '` ' + l.name + ' (' + l.percentage + '%) - ' + l.error).join('\n')}
 
-You should do an update now.`;
+You should do an update now.`);
         } else return 'Failed to use tha API: ' + JSON.stringify(res.body.response);
     }
 }
