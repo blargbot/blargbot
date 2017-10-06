5aaad2492d41260df6a5ca125d41c57241668c4b
stupid cat <cat@blargbot.xyz>
2017-10-06 12:32:21 -0600

    fix that

--- Frontend/plugins/i18n.js
+++ Frontend/plugins/i18n.js
@@ -10,7 +10,7 @@ export default ({ app, isClient, store }) => {
         locale: store.state.locale,
         fallbackLocale: 'en',
         messages: {
-            'en': require('~/../Locale/en.json')
+            'en': require('~/../Locale/en/website.json')
         }
     });
 };
