23af4b96ac2f0933ad00ddd0c14196fb5eb7aa76
stupid cat <cat@blargbot.xyz>
2017-10-14 18:46:43 -0600

    performance updates, hopefully

--- discord.js
+++ discord.js
@@ -2,7 +2,7 @@
  * @Author: stupid cat
  * @Date: 2017-05-07 19:31:12
  * @Last Modified by: stupid cat
- * @Last Modified time: 2017-10-04 10:48:23
+ * @Last Modified time: 2017-10-14 18:39:17
  *
  * This project uses the AGPLv3 license. Please read the license file before using/adapting any of the code.
  */
@@ -49,14 +49,15 @@ e.init = async function (v, em) {
         autoReconnect: true,
         disableEveryone: true,
         disableEvents: {
-            TYPING_START: true
+            TYPING_START: true,
+            PRESENCE_UPDATE: true,
+            VOICE_STATE_UPDATE: true
         },
-        getAllUsers: true,
         maxShards: config.discord.shards || 1,
         restMode: true,
         defaultImageFormat: 'png',
         defaultImageSize: 512,
-        messageLimit: 1
+        messageLimit: 0
     });
     global.bot = bot;
 
