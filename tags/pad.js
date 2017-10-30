b55bc5fe397b485865c4b4e14e5b1103ccdeb9e0
stupid cat <kirkbenm@gmail.com>
2017-10-30 12:15:05 -0600

    i hate everything

--- tags/pad.js
+++ tags/pad.js
@@ -2,7 +2,7 @@
  * @Author: stupid cat
  * @Date: 2017-05-07 18:50:26
  * @Last Modified by: stupid cat
- * @Last Modified time: 2017-10-30 12:13:29
+ * @Last Modified time: 2017-10-30 12:15:02
  *
  * This project uses the AGPLv3 license. Please read the license file before using/adapting any of the code.
  */
@@ -37,7 +37,7 @@ e.execute = async function (params) {
         switch (args1.toLowerCase()) {
             case 'right':
                 {
-                    replaceString = args2.substr(0, args3.length) + args3;
+                    replaceString = args2.substr(0, args2.length - args3.length) + args3;
                     break;
                 }
             case 'left':
