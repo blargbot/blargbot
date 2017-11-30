d93fec041abe13818e9445b451d8dcfc63032ddf
Vap0r1ze <superdash993@gmail.com>
2017-11-30 18:03:53 -0500

    Tags: add regex string matching

---
+++ tags/match.js
@@ -0,0 +1,37 @@
+var e = module.exports = {};
+
+e.init = () => {
+    e.category = bu.TagType.ARRAY;
+};
+
+e.requireCtx = require;
+
+e.isTag = true;
+e.name = `match`;
+e.args = `&lt;text&gt; &lt;regex&gt;`;
+e.usage = `{match;text;regex}`;
+e.desc = `Returns an array of everything in <code>text</code> that matches <code>regex</code>.`;
+e.exampleIn = `{match;I have $1 and 25 cents;/\\d/}`;
+e.exampleOut = `["1", "25"]`;
+
+e.execute = async function(params) {
+    for (let i = 1; i < params.args.length; i++) {
+        params.args[i] = await bu.processTagInner(params, i);
+    }
+    let words = params.words;
+    var replaceString = '';
+    var replaceContent = false;
+    let args = params.args;
+    if (args.length >= 2) {
+        let regex = bu.createRegExp(args[2]);
+        let array = args[1].match(regex);
+        replaceString = bu.serializeTagArray(array);
+    } else {
+        replaceString = await bu.tagProcessError(params, '`Not enough arguments`');
+    }
+    return {
+        terminate: params.terminate,
+        replaceString: replaceString,
+        replaceContent: replaceContent
+    };
+};
