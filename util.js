b7180b2906984101448e6c2b21e327eab8033d45
stupid cat <kirkbenm@gmail.com>
2017-12-05 16:43:36 -0700

    aaa

--- util.js
+++ util.js
@@ -2,7 +2,7 @@
  * @Author: stupid cat
  * @Date: 2017-05-07 19:37:01
  * @Last Modified by: stupid cat
- * @Last Modified time: 2017-12-05 16:24:22
+ * @Last Modified time: 2017-12-05 16:43:28
  *
  * This project uses the AGPLv3 license. Please read the license file before using/adapting any of the code.
  */
@@ -10,10 +10,12 @@
 var bu = module.exports = {};
 
 const loggerModule = require('./logger.js');
+const EventEmitter = require('eventemitter3');
 
 loggerModule.init();
 global.cluster = require('./cluster.js');
 
+bu.emitter = new EventEmitter();
 bu.init = () => {
     global.r = require('rethinkdbdash')({
         host: config.db.host,
