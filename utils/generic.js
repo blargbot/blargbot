e6051038ea04a6d0a59a265595e94f6bf014f1e7
stupid cat <kirkbenm@gmail.com>
2017-12-05 16:32:22 -0700

    aaaaaaa

--- utils/generic.js
+++ utils/generic.js
@@ -2,11 +2,12 @@
  * @Author: stupid cat
  * @Date: 2017-05-07 19:22:33
  * @Last Modified by: stupid cat
- * @Last Modified time: 2017-12-05 16:29:42
+ * @Last Modified time: 2017-12-05 16:32:19
  *
  * This project uses the AGPLv3 license. Please read the license file before using/adapting any of the code.
  */
-bu.emitter = new dep.EventEmitter();
+const EventEmitter = require('eventemitter3');
+bu.emitter = new EventEmitter();
 
 bu.compareStats = (a, b) => {
     if (a.uses < b.uses)
