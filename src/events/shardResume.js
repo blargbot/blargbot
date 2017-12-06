0ba21f571333880c75a243068024e329f79f177e
stupid cat <kirkbenm@gmail.com>
2017-12-06 10:42:14 -0700

    more logging changes

--- src/events/shardResume.js
+++ src/events/shardResume.js
@@ -2,12 +2,11 @@
  * @Author: stupid cat
  * @Date: 2017-05-07 18:23:34
  * @Last Modified by: stupid cat
- * @Last Modified time: 2017-05-07 18:23:34
+ * @Last Modified time: 2017-12-06 10:40:13
  *
  * This project uses the AGPLv3 license. Please read the license file before using/adapting any of the code.
  */
 
 bot.on('shardResume', async function (id) {
-    let shard = bot.shards.get(id);
-    console.info(`${id} Resumed! G:${shard.guildCount}`);
+    console.shardi(`Resumed! G:${shard.guildCount}`);
 });
