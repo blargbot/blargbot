adb9d506a822ed7979b7b83762b71768cf83daf5
stupid cat <kirkbenm@gmail.com>
2017-12-15 14:50:04 -0700

    fix stackless errors

--- src/tags/repeat.js
+++ src/tags/repeat.js
@@ -2,7 +2,7 @@
  * @Author: stupid cat
  * @Date: 2017-05-07 18:54:06
  * @Last Modified by: stupid cat
- * @Last Modified time: 2017-11-19 17:52:38
+ * @Last Modified time: 2017-12-15 14:47:15
  *
  * This project uses the AGPLv3 license. Please read the license file before using/adapting any of the code.
  */
