be109d0217ac24ab1de7fac6cb5fa5350e837785
Titansmasher <dannyjmay97@gmail.com>
2018-02-26 16:02:35 +0000

    Updated caplitalize.js to use TagBuilder

--- src/tags/capitalize.js
+++ src/tags/capitalize.js
@@ -7,41 +7,21 @@
  * This project uses the AGPLv3 license. Please read the license file before using/adapting any of the code.
  */
 
-var e = module.exports = {};
+const Builder = require('../structures/TagBuilder');
 
-e.init = () => {
-    e.category = bu.TagType.COMPLEX;
-};
-
-e.requireCtx = require;
-
-e.isTag = true;
-e.name = 'capitalize';
-e.args = '&lt;text&gt; [lower]';
-e.usage = '{capitalize;text;lower}';
-e.desc = 'Capitalizes the first letter of `text`. If `lower` is specified the rest of the text will be lowercase';
-e.exampleIn = '{capitalize;hello world!}';
-e.exampleOut = 'Hello world!';
-
-e.execute = async function (params) {
-    for (let i = 1; i < params.args.length; i++) {
-        params.args[i] = await bu.processTagInner(params, i);
-    }
-    var replaceString = '';
-    var replaceContent = false;
-    if (params.args[1]) {
-        if (params.args[2]) {
-            replaceString = params.args[1][0].toUpperCase() + params.args[1].substr(1).toLowerCase();
-        } else {
-            replaceString = params.args[1][0].toUpperCase() + params.args[1].substr(1);
-        }
-    } else {
-        replaceString = await bu.tagProcessError(params, '`Not enough arguments`');
-    }
-
-    return {
-        terminate: params.terminate,
-        replaceString: replaceString,
-        replaceContent: replaceContent
-    };
-};
+module.exports =
+  Builder.ComplexTag('capitalize')
+    .withArgs(b =>
+      b.require('text').optional('lower')
+    ).withDesc('Capitalizes the first letter of `text`. If `lower` is specified the rest of the text will be lowercase')
+    .withExample(
+      '{capitalize;hello world!}',
+      'Hello world!'
+    ).beforeExecute(Builder.defaults.processAllSubtags)
+    .whenArgs('1', Builder.defaults.notEnoughArguments)
+    .whenArgs('2-3', async params => {
+      if (params.args[2])
+        return params.args[1][0].toUpperCase() + params.args[1].substr(1).toLowerCase();
+      return params.args[1][0].toUpperCase() + params.args[1].substr(1);
+    }).whenDefault(Builder.defaults.tooManyArguments)
+    .build()
