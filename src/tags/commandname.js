a89f86b2e179d8c3ca1eede0c820b346886dbc10
Titansmasher <dannyjmay97@gmail.com>
2018-02-26 19:59:30 +0000

    Updated commandname.js to use TagBuilder

--- src/tags/commandname.js
+++ src/tags/commandname.js
@@ -7,38 +7,14 @@
  * This project uses the AGPLv3 license. Please read the license file before using/adapting any of the code.
  */
 
-var e = module.exports = {};
+const Builder = require('../structures/TagBuilder');
 
-e.init = () => {
-    e.category = bu.TagType.SIMPLE;
-};
-
-e.requireCtx = require;
-
-e.isTag = true;
-e.name = `commandname`;
-e.args = ``;
-e.usage = `{commandname}`;
-e.desc = `Gets the name of the current tag or custom command. Will throw an error in other instances.`;
-e.exampleIn = `This command is {commandname}`;
-e.exampleOut = `This command is test`;
-
-e.execute = async function (params) {
-    for (let i = 1; i < params.args.length; i++) {
-        params.args[i] = await bu.processTagInner(params, i);
-    }
-    let args = params.args,
-        fallback = params.fallback;
-    var replaceString = '';
-    var replaceContent = false;
-    console.verbose(params.tagName);
-    if (params.tagName)
-        replaceString = params.tagName;
-    else replaceString = await bu.tagProcessError(params, '`Not a command`');
-
-    return {
-        terminate: params.terminate,
-        replaceString: replaceString,
-        replaceContent: replaceContent
-    };
-};
+module.exports =
+  Builder.SimpleTag('commandname')
+    .withDesc('Gets the name of the current tag or custom command. Will throw an error in other instances.')
+    .withExample(
+      'This command is {commandname}',
+      'This command is test'
+    ).whenArgs('1', async params => params.tagName || await bu.tagProcessError(params, '`Not a command`'))
+    .whenDefault(Builder.defaults.tooManyArguments)
+    .build();
