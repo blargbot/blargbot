e28b3297a490410ea3f7409e4746cb94a8004a86
Titansmasher <dannyjmay97@gmail.com>
2018-02-26 02:44:33 +0000

    Updated abs.js to use TagBuilder

--- src/tags/abs.js
+++ src/tags/abs.js
@@ -7,43 +7,25 @@
  * This project uses the AGPLv3 license. Please read the license file before using/adapting any of the code.
  */
 
-var e = module.exports = {};
+const Builder = require('../structures/TagBuilder');
 
-e.init = () => {
-    e.category = bu.TagType.COMPLEX;
-};
-
-e.requireCtx = require;
-
-e.isTag = true;
-e.name = `abs`;
-e.args = `&lt;number&gt;`;
-e.usage = `{abs;number}`;
-e.desc = `Gets the absolute value of a number`;
-e.exampleIn = `{abs;-535}`;
-e.exampleOut = `535`;
-
-e.execute = async function (params) {
-    for (let i = 1; i < params.args.length; i++) {
-        params.args[i] = await bu.processTagInner(params, i);
-    }
-    let args = params.args,
-        fallback = params.fallback;
-    var replaceString = '';
-    var replaceContent = false;
-    if (args[1]) {
-        var asNumber = parseFloat(args[1]);
-        if (!isNaN(asNumber)) {
-            replaceString = Math.abs(asNumber);
-        } else {
-            replaceString = await bu.tagProcessError(params, '`Not a number`');
-        }
-    } else {
-        replaceString = await bu.tagProcessError(params, '`Not enough arguments`');
-    }
-    return {
-        terminate: params.terminate,
-        replaceString: replaceString,
-        replaceContent: replaceContent
-    };
-};
+module.exports =
+    new Builder()
+        .withCategory(bu.TagType.COMPLEX)
+        .withName('abs')
+        .withArgs(b =>
+            b.require('number')
+        ).withDesc('Gets the absolute value of a number')
+        .withExample('{abs;-535}', '535')
+        .beforeExecute(Builder.defaults.processAllSubtags)
+        .whenArgs('2', async params => {
+            let asNumber = parseFloat(params.args[1]);
+            if (!isNaN(asNumber)) {
+                return Math.abs(asNumber);
+            } else {
+                return await bu.tagProcessError(params, '`Not a number`');
+            }
+        })
+        .whenArgs('<2', Builder.defaults.notEnoughArguments)
+        .whenArgs('>2', Builder.defaults.tooManyArguments)
+        .build();
