570d3cf1a7be97601a390de96afb22d3dd5389b2
Titansmasher <dannyjmay97@gmail.com>
2018-03-03 12:37:43 +0000

    Enabled arrays being defined using ' or "

--- src/utils/tag.js
+++ src/utils/tag.js
@@ -26,6 +26,7 @@ bu.serializeTagArray = function (array, varName) {
 bu.deserializeTagArray = function (value) {
     try {
         let obj = JSON.parse(value
+            .replace(/'/g, '"')
             .replace(new RegExp(bu.specialCharBegin + 'LB' + bu.specialCharEnd, "g"), '{')
             .replace(new RegExp(bu.specialCharBegin + 'RB' + bu.specialCharEnd, "g"), '}'));
         if (Array.isArray(obj)) obj = {
