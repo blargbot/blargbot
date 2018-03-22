10c2edcfd911369bc844cdbda7db75b251e77a07
Titansmasher <dannyjmay97@gmail.com>
2018-03-22 20:52:49 +0000

    Removed duplicated function

--- src/utils/generic.js
+++ src/utils/generic.js
@@ -843,25 +843,6 @@ bu.canExecuteCommand = async function (msg, commandName, quiet) {
     }
 };
 
-bu.isUserStaff = async function (userId, guildId) {
-    let guild = bot.guilds.get(guildId);
-    if (!guild) return false;
-    let member = guild.members.get(userId);
-    if (!member) return false;
-
-    if (guild.ownerID == userId) return true;
-    if (member.permission.has('administrator')) return true;
-
-    let storedGuild = await bu.getGuild(guildId);
-    if (storedGuild && storedGuild.settings && storedGuild.settings.permoverride) {
-        let allow = storedGuild.settings.staffperms || bu.defaultStaff;
-        if (bu.comparePerms(bot.guilds.get(guildId).members.get(userId), allow)) {
-            return true;
-        }
-    }
-    return false;
-};
-
 bu.shuffle = (array) => {
     let i = 0,
         j = 0,
