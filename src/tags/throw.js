661647271dd42833bcee052a809747d66a42977c
stupid cat <kirkbenm@gmail.com>
2018-03-29 14:40:30 -0600

    another fix

--- src/tags/throw.js
+++ src/tags/throw.js
@@ -2,7 +2,7 @@
  * @Author: stupid cat
  * @Date: 2017-05-07 19:05:37
  * @Last Modified by: stupid cat
- * @Last Modified time: 2017-05-07 19:05:37
+ * @Last Modified time: 2018-03-29 14:40:10
  *
  * This project uses the AGPLv3 license. Please read the license file before using/adapting any of the code.
  */
@@ -17,6 +17,6 @@ module.exports =
             '{throw;Custom Error}',
             '\u200B`Custom Error`\u200B'
         )
-        .whenArgs('0-1', async (subtag, context, args) => Builder.util.error(args[0] || 'A custom error occurred'))
+        .whenArgs('0-1', async (subtag, context, args) => Builder.util.error(subtag, context, args[0] || 'A custom error occurred'))
         .whenDefault(Builder.errors.tooManyArguments)
         .build();
