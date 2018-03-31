a29af0527e0d2b266459892f4e925a85ec64cb15
stupid cat <kirkbenm@gmail.com>
2018-03-31 14:07:24 -0600

    fix comments

--- src/tags/comment.js
+++ src/tags/comment.js
@@ -2,7 +2,7 @@
  * @Author: stupid cat
  * @Date: 2017-05-07 18:30:45
  * @Last Modified by: stupid cat
- * @Last Modified time: 2017-05-07 18:30:45
+ * @Last Modified time: 2018-03-31 14:07:18
  *
  * This project uses the AGPLv3 license. Please read the license file before using/adapting any of the code.
  */
@@ -17,4 +17,5 @@ module.exports =
             'This is a sentence. {//;This is a comment.}',
             'This is a sentence.'
         ).whenDefault(async _ => '')
+        .resolveArgs(-1)
         .build();
