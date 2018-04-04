3dfce2591bc4452c366b215ca46baaf9281544f8
stupid cat <kirkbenm@gmail.com>
2018-04-03 19:39:17 -0600

    fix choose being 1-indexed

--- src/tags/choose.js
+++ src/tags/choose.js
@@ -2,7 +2,7 @@
  * @Author: stupid cat
  * @Date: 2017-05-07 18:30:33
  * @Last Modified by: stupid cat
- * @Last Modified time: 2017-05-07 18:30:33
+ * @Last Modified time: 2018-04-03 19:39:05
  *
  * This project uses the AGPLv3 license. Please read the license file before using/adapting any of the code.
  */
@@ -28,6 +28,6 @@ module.exports =
             if (index < 0)
                 return Builder.util.error(subtag, context, 'Choice cannot be negative');
 
-            return await bbEngine.execute(args[index], context);
+            return await bbEngine.execute(args[index + 1], context);
         })
         .build();
