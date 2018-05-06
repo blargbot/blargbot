a816098ae70a2aedfb1d8c67ef770c103a6db996
Titansmasher <dannyjmay97@gmail.com>
2018-05-06 16:22:35 +0100

    Added regexsplit

---
+++ src/tags/regexsplit.js
@@ -0,0 +1,31 @@
+/*
+ * @Author: stupid cat
+ * @Date: 2017-05-07 18:51:46
+ * @Last Modified by: stupid cat
+ * @Last Modified time: 2017-10-17 11:59:38
+ *
+ * This project uses the AGPLv3 license. Please read the license file before using/adapting any of the code.
+ */
+
+const Builder = require('../structures/TagBuilder');
+
+module.exports =
+    Builder.AutoTag('regexsplit')
+        .withArgs(a => [a.require('text'), a.require('regex')])
+        .withDesc('Splits the given text using the given `regex` as the split rule')
+        .withExample(
+            '{regexsplit;Hello      there, I       am hungry;/[\\s,]+/}',
+            '["Hello","there","I","am","hungry"]'
+        ).resolveArgs(0)
+        .whenArgs('0-1', Builder.errors.notEnoughArguments)
+        .whenArgs(2, async function (subtag, context, args) {
+            let regex;
+            try {
+                regex = bu.createRegExp(args[1].content);
+            } catch (e) {
+                return Builder.util.error(subtag, context, e);
+            }
+
+            return JSON.stringify(args[0].split(regex));
+        }).whenDefault(Builder.errors.tooManyArguments)
+        .build();
