559ba626fcf83fa024299762de5e9fa83bcabb8a
HunteRoi <32441291+HunteRoi@users.noreply.github.com>
2018-05-07 21:30:00 +0200

    Update blacklist.js

--- src/dcommands/blacklist.js
+++ src/dcommands/blacklist.js
@@ -6,32 +6,35 @@ class BlacklistCommand extends BaseCommand {
             name: 'blacklist',
             category: bu.CommandType.ADMIN,
             usage: 'blacklist [channel]',
-            info: 'Blacklists the current channel, or the first channel that you mention. The bot will not respond until you do `blacklist` again.'
+            info: 'Blacklists the current channel, or the channel(s) that you mention. The bot will not respond until you do `blacklist` again.'
         });
     }
 
     async execute(msg, words, text) {
-    let channelid = msg.channel.id;
-    if (msg.channelMentions.length > 0) channelid = msg.channelMentions[0];
+        let channelids = [];
+        if (msg.channelMentions.length <= 0) channelids.push(msg.channel.id);
+        else channelids = msg.channelMentions;
 
-    let storedGuild = await bu.getGuild(msg.guild.id);
-    let channel = storedGuild.channels && storedGuild.channels.hasOwnProperty(channelid) ?
-        storedGuild.channels[channelid] : {
-            nsfw: false
-        };
+        let storedGuild = await bu.getGuild(msg.guild.id);
+        for (let channelid of channelids) {
+            let channel = storedGuild.channels && storedGuild.channels.hasOwnProperty(channelid) ?
+                storedGuild.channels[channelid] : {
+                    nsfw: false
+                };
 
-    if (channel.blacklisted) {
-        channel.blacklisted = false;
-        bu.send(msg, '**' + bot.getChannel(channelid).name + '** is no longer blacklisted.');
-    } else {
-        channel.blacklisted = true;
-        bu.send(msg, '**' + bot.getChannel(channelid).name + '** is now blacklisted.');
-    }
+            if (channel.blacklisted) {
+                channel.blacklisted = false;
+                bu.send(msg, '**' + bot.getChannel(channelid).name + '** is no longer blacklisted.');
+            } else {
+                channel.blacklisted = true;
+                bu.send(msg, '**' + bot.getChannel(channelid).name + '** is now blacklisted.');
+            }
 
-    storedGuild.channels[channelid] = channel;
-    r.table('guild').get(msg.guild.id).update({
-        channels: storedGuild.channels
-    }).run();
+            storedGuild.channels[channelid] = channel;
+        }
+        r.table('guild').get(msg.guild.id).update({
+            channels: storedGuild.channels
+        }).run();
     }
 }
 
