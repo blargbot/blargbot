1afdd8e0047538d76aa48abc4818b134c2e9423c
stupid cat <kirkbenm@gmail.com>
2018-05-11 13:44:59 -0600

    make guildicon use pngs

--- src/tags/guildicon.js
+++ src/tags/guildicon.js
@@ -2,7 +2,7 @@
  * @Author: stupid cat
  * @Date: 2017-05-07 18:47:06
  * @Last Modified by: stupid cat
- * @Last Modified time: 2017-05-07 18:47:06
+ * @Last Modified time: 2018-05-11 13:44:50
  *
  * This project uses the AGPLv3 license. Please read the license file before using/adapting any of the code.
  */
@@ -13,9 +13,9 @@ module.exports =
     Builder.AutoTag('guildicon')
         .withDesc('Returns the icon of the current guild.')
         .withExample(
-            'The guild\'s icon is {guildicon}',
-            'The guild\'s icon is (icon url)'
+        'The guild\'s icon is {guildicon}',
+        'The guild\'s icon is (icon url)'
         )
-        .whenArgs(0, async (_, context) => `https://cdn.discordapp.com/icons/${context.guild.id}/${context.guild.icon}.jpg`)
+        .whenArgs(0, async (_, context) => context.guild.iconURL)
         .whenDefault(Builder.errors.tooManyArguments)
         .build();
