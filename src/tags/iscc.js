4335d4ef25ee0113f991da1c89af50b6a46b074c
stupid cat <kirkbenm@gmail.com>
2018-05-16 09:46:25 -0600

    fix iscc in tag test

--- src/tags/iscc.js
+++ src/tags/iscc.js
@@ -2,7 +2,7 @@
  * @Author: stupid cat
  * @Date: 2017-05-07 18:50:20
  * @Last Modified by: stupid cat
- * @Last Modified time: 2017-05-07 18:50:20
+ * @Last Modified time: 2018-05-16 09:46:15
  *
  * This project uses the AGPLv3 license. Please read the license file before using/adapting any of the code.
  */
@@ -13,9 +13,9 @@ module.exports =
     Builder.AutoTag('iscc')
         .withDesc('Checks if the tag is being run from within a cc. Returns a boolean (`true` or `false`)')
         .withExample(
-            '{if;{iscc};{dm;{userid};You have mail!};Boo, this only works in cc\'s}',
-            'Boo, this only works in cc\'s'
+        '{if;{iscc};{dm;{userid};You have mail!};Boo, this only works in cc\'s}',
+        'Boo, this only works in cc\'s'
         )
-        .whenArgs(0, async (_, context) => context.isCC)
+        .whenArgs(0, async (_, context) => context.isCC === true)
         .whenDefault(Builder.errors.tooManyArguments)
         .build();
