b0af0b3edbbb59305cb05f74e93bcf7dcca370c7
titansmasher <dannyjmay97@gmail.com>
2018-06-16 11:07:50 +0100

    Add {output} subtag

---
+++ src/tags/output.js
@@ -0,0 +1,24 @@
+/*
+ * @Author: stupid cat
+ * @Date: 2017-05-07 18:50:20
+ * @Last Modified by: stupid cat
+ * @Last Modified time: 2017-05-07 18:50:20
+ *
+ * This project uses the AGPLv3 license. Please read the license file before using/adapting any of the code.
+ */
+
+const Builder = require('../structures/TagBuilder');
+
+module.exports =
+    Builder.AutoTag('output')
+        .withArgs(a => a.optional('text'))
+        .withDesc('WIP')
+        .withExample(
+            '{output;Hello!}',
+            'Hello!'
+        )
+        .whenArgs('0-1', async function (_, context, args) {
+            return (await context.sendOutput(args[0])).id;
+        })
+        .whenDefault(Builder.errors.tooManyArguments)
+        .build();
