7a7bd3e6731d69f0dd38ce93d1724670f3bf97d1
titansmasher <dannyjmay97@gmail.com>
2018-06-19 00:45:15 +0100

    Allow deletion of embeds/text in edit

--- src/tags/edit.js
+++ src/tags/edit.js
@@ -13,7 +13,7 @@ module.exports =
     Builder.APITag('edit')
         .withArgs(a => [a.optional('channelId'), a.require('messageId'), a.require([a.optional('text'), a.optional('embed')])])
         .withDesc('Edits `messageId` in `channelId` to say `text` or `embed`. ' +
-            'Atleast one of `text` and `embed` is required. ' +
+            'Atleast one of `text` and `embed` is required. To delete the message text or the embed, enter `_delete`' +
             'If `channelId` is not supplied, it defaults to the current channel.\n' +
             'Please note that `embed` is the JSON for an embed object, don\'t put the `{embed}` subtag there, as nothing will show.\n' +
             'Only messages created by the bot may be edited')
@@ -22,22 +22,23 @@ module.exports =
             '(the message got edited idk how to do examples for this)'
         )
         .whenArgs('0-1', Builder.errors.notEnoughArguments)
-        .whenArgs(2, async function(subtag, context, args) { //args = [<messageId>,<text|embed>]
-            let message = args[1],
+        .whenArgs(2, async function (subtag, context, args) { //args = [<messageId>,<text|embed>]
+            let text = args[1],
                 embed = bu.parseEmbed(args[1]);
 
             if (embed != null && !embed.malformed)
-                message = undefined; //args = [<messageId>,<embed>]
+                text = undefined; //args = [<messageId>,<embed>]
             else
                 embed = undefined; //args = [<messageId>,<text>]
 
-            return await this.runEdit(subtag, context, context.channel, args[0], message, embed);
+            return await this.runEdit(subtag, context, context.channel, args[0], text, embed);
         })
-        .whenArgs(3, async function(subtag, context, args) { //args = [(<messageId>,<text>,<embed>)|(<channelid>,<messageId>,<text|embed>)]
+        .whenArgs(3, async function (subtag, context, args) { //args = [(<messageId>,<text>,<embed>)|(<channelid>,<messageId>,<text|embed>)]
             let channel = bu.parseChannel(args[0], true);
             if (channel == null) { //args = [<messageId>,<text>,<embed>]
-                let text = args[1],
+                let text = args[1] || undefined,
                     embed = bu.parseEmbed(args[2]);
+                if (!embed || embed.malformed) embed = args[2];
                 return await this.runEdit(subtag, context, context.channel, args[0], text, embed);
             }
 
@@ -45,20 +46,21 @@ module.exports =
                 embed = bu.parseEmbed(args[2]);
 
             if (embed != null && !embed.malformed)
-                text = null; //args = [<channelId>,<messageId>,<embed>]
+                text = undefined; //args = [<channelId>,<messageId>,<embed>]
             else
-                embed = null; //args = [<channelId>,<messageId>,<text>]
+                embed = undefined; //args = [<channelId>,<messageId>,<text>]
             return await this.runEdit(subtag, context, channel, args[1], text, embed);
         })
-        .whenArgs(4, async function(subtag, context, args) { //args = [<channelId>,<messageId>,<text>,<embed>]
+        .whenArgs(4, async function (subtag, context, args) { //args = [<channelId>,<messageId>,<text>,<embed>]
             let channel = bu.parseChannel(args[0], true),
                 messageId = args[1],
-                text = args[2],
-                embed = bu.parseEmbed(args[3]);
+                text = args[2] || undefined,
+                embed = bu.parseEmbed(args[3]) || args[3];
+            if (!embed || embed.malformed) embed = args[3];
             return await this.runEdit(subtag, context, channel, messageId, text, embed);
         })
         .whenDefault(Builder.errors.tooManyArguments)
-        .withProp('runEdit', async function(subtag, context, channel, messageId, text, embed) {
+        .withProp('runEdit', async function (subtag, context, channel, messageId, text, embed) {
             if (!(await context.isStaff || context.ownsMessage(messageId)))
                 return Builder.util.error(subtag, context, 'Author must be staff to edit unrelated messages');
 
@@ -80,14 +82,30 @@ module.exports =
             if (message == null) return Builder.errors.noMessageFound(subtag, context);
             if (message.author.id != bot.user.id) return Builder.util.error(subtag, context, 'I must be the message author');
 
+            text = text || message.content;
+            embed = embed || message.embeds[0];
+
+            if (text == '_delete') text = '';
+            if (embed == '_delete') embed = null;
+            if (typeof embed === 'string') embed = undefined;
+
             if ((text == null || text.trim() == '') && embed == null)
                 return Builder.util.error(subtag, context, 'Message cannot be empty');
+
+            // For text:
+            // '' = empty
+            // null = 'null'
+            // undefined = dont change
+
+            // For embed:
+            // null = remove
+            // undefined = dont change
 
             try {
                 if (message.edit)
                     message.edit({
-                        content: text || message.content,
-                        embed: embed || message.embeds[0]
+                        content: text,
+                        embed: embed
                     });
             } catch (err) { }
         })
