97c09c6d94dfc6b4ed32f5a948984c1ba8b48400
titansmasher <dannyjmay97@gmail.com>
2018-06-27 22:49:44 +0100

    remove babel

--- src/core/dep.js
+++ src/core/dep.js
@@ -8,7 +8,6 @@
  */
 
 var e = module.exports = {
-    babel: require('babel-core'),
     brainfuck: new (require('brainfuck-node'))(),
     BigNumber: require('big-number'),
     url: require('url')
