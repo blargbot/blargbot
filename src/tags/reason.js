86c6cc5b64d1cb9bf951ea947d64a5d33487ec20
stupid cat <kirkbenm@gmail.com>
2018-07-05 15:20:38 -0600

    memes

--- src/tags/reason.js
+++ src/tags/reason.js
@@ -2,7 +2,7 @@
  * @Author: stupid cat
  * @Date: 2017-05-07 18:50:20
  * @Last Modified by: stupid cat
- * @Last Modified time: 2018-07-05 15:17:40
+ * @Last Modified time: 2018-07-05 15:20:35
  *
  * This project uses the AGPLv3 license. Please read the license file before using/adapting any of the code.
  */
@@ -11,7 +11,7 @@ const Builder = require('../structures/TagBuilder');
 
 module.exports =
     Builder.AutoTag('reason')
-        .withArgs(a => [a.required('reason')])
+        .withArgs(a => [a.require('reason')])
         .withDesc('Sets the reason for the next API call (ex. roleadd, roleremove, ban, etc.)')
         .withExample(
         '{reason;This will show up in the audit logs!}{roleadd;111111111111}',
