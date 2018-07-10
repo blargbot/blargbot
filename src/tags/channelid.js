632a7d68e7283b17da7666baef1cb9f11e3ca122
Zoomah <ronny.luettke@live.de>
2018-07-10 06:52:59 +0200

    Update channelid.js

--- src/tags/channelid.js
+++ src/tags/channelid.js
@@ -21,7 +21,7 @@ module.exports =
         )
         .whenArgs(0, async (_, context) => context.channel.id)
         .whenArgs('1-2', async (_, context, args) => {
-            let ch = context.guild.channels.find(c => c.name == args[0]);
+            let ch = bu.parseChannel(args[0], true);
             let quiet = bu.isBoolean(context.scope.quiet) ? context.scope.quiet : !!args[1]
             return ch ? ch.id : quiet ? '' : '`Channel not found`';
         })
