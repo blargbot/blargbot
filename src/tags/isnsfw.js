673e922f495ce4ec0ad6490cf0088599d08bd106
Zoomah <ronny.luettke@live.de>
2018-07-10 06:51:00 +0200

    Update isnsfw.js

--- src/tags/isnsfw.js
+++ src/tags/isnsfw.js
@@ -11,18 +11,20 @@ const Builder = require('../structures/TagBuilder');
 
 module.exports =
     Builder.AutoTag('isnsfw')
-        .withArgs(a => a.optional('channelId'))
+        .withArgs(a => a.optional('channelId'), a => [a.optional('quiet')])
         .withDesc('Checks if `channelId` is a NSFW channel. `channelId` defaults to the current channel')
         .withExample(
             '{if;{isnsfw};Spooky nsfw stuff;fluffy bunnies}',
             'fluffy bunnies'
         )
-        .whenArgs('0-1', async function (subtag, context, args) {
+        .whenArgs('0-2', async function (subtag, context, args) {
             let channel = context.channel;
             if (args[0])
                 channel = bu.parseChannel(args[0], true);
 
-            if (channel == null) return Builder.errors.noChannelFound(subtag, context);
+            let quiet = bu.isBoolean(context.scope.quiet) ? context.scope.quiet : !!args[1]
+            
+            if (channel == null) return quiet ? '' : Builder.errors.noChannelFound(subtag, context);
 
             return await bu.isNsfwChannel(channel.id);
         })
