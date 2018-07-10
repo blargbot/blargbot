9ee7b8952ee8c14f22eaf0105c5318ccefc453b8
Zoomah <ronny.luettke@live.de>
2018-07-10 06:22:12 +0200

    Update channels.js

--- src/tags/channels.js
+++ src/tags/channels.js
@@ -11,11 +11,17 @@ const Builder = require('../structures/TagBuilder');
 
 module.exports =
     Builder.APITag('channels')
+        .withArgs(a => [a.optional('categoryid')], a => [a.optional('quiet')])
         .withDesc('Returns an array of channel IDs of the channels on the current guild.')
         .withExample(
             'This guild has {length;{guildchannels}} channels.',
             'This guild has 123 channels.'
         )
         .whenArgs(0, async (_, context) => JSON.stringify(context.guild.channels.map(c => c.id)))
+        .whenArgs('1-2', async (_, context, args) => {
+            let quiet = bu.isBoolean(context.scope.quiet) ? context.scope.quiet : !!args[1]
+            let ch == context.guild.channels.find(c => c.id == args[0]);
+            return (ch && ch.channels) ? JSON.stringify(ch.channels.map(c => c.id)) : quiet ? '' : '`Category not found`';
+        })
         .whenDefault(Builder.errors.tooManyArguments)
         .build();
