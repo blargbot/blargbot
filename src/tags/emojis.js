9e31f2cedfcd95e3c6275deeb6fb919aa29c814b
HunteRoi <32441291+HunteRoi@users.noreply.github.com>
2018-07-19 23:21:05 +0200

    Update emojis.js
    `{emojis}` returns all emojis of the current guild in an array looking like `[":name:id:", ":name:id:", "a:name:id:", ":name:id:", "a:name:id"]`.
    `{emojis;ROLEID}` returns all emojis of the current guild the role users may access (thus write in the chat) in an array the same way it does for the above example.

--- src/tags/emojis.js
+++ src/tags/emojis.js
@@ -2,7 +2,7 @@
  * @Author: zoomah
  * @Date: 2018-07-10 7:08:15
  * @Last Modified by: HunteRoi
- * @Last Modified time: 2018-07-19 18:30:00
+ * @Last Modified time: 2018-07-19 23:13:10
  *
  * This project uses the AGPLv3 license. Please read the license file before using/adapting any of the code.
  */
@@ -11,24 +11,21 @@ const Builder = require('../structures/TagBuilder');
 
 module.exports =
     Builder.APITag('emojis')
-        .withArgs(a => [a.optionnal('quiet')])
+        .withArgs(a => [a.optional('roleid')])
         .withDesc('Returns an array of emoji IDs of the current guild.' +
-            'If `quiet` is specified, if an emoji is restricted to at least one role, then the emoji simply get ignored.'
+            'If `roleid` is specified, returns all the emojis whitelisted for the provided role.'
         )
         .withExample(
             'This guild has {length;{emojis}} emojis.',
             'This guild has 23 emojis.'
         )
         .whenArgs(0, async (subtag, context, args) => {
-            let botRoles = context.guild.members.get(bot.user.id).roles.map(r => r.id);
-            let emojis = context.guild.emojis.map(e => {
-                if (botRoles.some(r => e.roles.includes(r))) `<${e.animated ? 'a' : ''}:${e.name}:${e.id}>`;
-            });
+            let emojis = context.guild.emojis.map(e => `${e.animated ? 'a' : ''}:${e.name}:${e.id}`);
             return JSON.stringify(emojis);
         })
         .whenArgs(1, async (subtag, context, args) => {
             let emojis = context.guild.emojis.map(e => {
-                if (e.roles === undefined) `<${e.animated ? 'a' : ''}:${e.name}:${e.id}>`;
+                if (e.roles.includes(args[1])) `${e.animated ? 'a' : ''}:${e.name}:${e.id}`;
             });
             return JSON.stringify(emojis);
         })
