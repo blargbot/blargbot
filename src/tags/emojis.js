71880b4b8d942b4bc3c19bca5e6c6e1302ca9d64
HunteRoi <32441291+HunteRoi@users.noreply.github.com>
2018-07-19 18:37:53 +0200

    Ignoring role-restricted emojis

--- src/tags/emojis.js
+++ src/tags/emojis.js
@@ -1,8 +1,8 @@
 /*
  * @Author: zoomah
  * @Date: 2018-07-10 7:08:15
- * @Last Modified by: stupid cat
- * @Last Modified time: 2018-07-18 12:25:40
+ * @Last Modified by: HunteRoi
+ * @Last Modified time: 2018-07-19 18:30:00
  *
  * This project uses the AGPLv3 license. Please read the license file before using/adapting any of the code.
  */
@@ -11,13 +11,25 @@ const Builder = require('../structures/TagBuilder');
 
 module.exports =
     Builder.APITag('emojis')
-        .withDesc('Returns an array of IDs of the emojis in the server.')
+        .withArgs(a => [a.optionnal('quiet')])
+        .withDesc('Returns an array of emoji IDs of the current guild.' +
+            'If `quiet` is specified, if an emoji is restricted to at least one role, then the emoji simply get ignored.'
+        )
         .withExample(
-        'This guild has {length;{emojis}} emojis.',
-        'This guild has 23 emojis.'
+            'This guild has {length;{emojis}} emojis.',
+            'This guild has 23 emojis.'
         )
         .whenArgs(0, async (subtag, context, args) => {
-            let emojis = context.guild.emojis.map(e => `<${e.animated ? 'a' : ''}:${e.name}:${e.id}>`);
+            let botRoles = context.guild.members.get(bot.user.id).roles.map(r => r.id);
+            let emojis = context.guild.emojis.map(e => {
+                if (botRoles.some(r => e.roles.includes(r))) `<${e.animated ? 'a' : ''}:${e.name}:${e.id}>`;
+            });
+            return JSON.stringify(emojis);
+        })
+        .whenArgs(1, async (subtag, context, args) => {
+            let emojis = context.guild.emojis.map(e => {
+                if (e.roles === undefined) `<${e.animated ? 'a' : ''}:${e.name}:${e.id}>`;
+            });
             return JSON.stringify(emojis);
         })
         .whenDefault(Builder.errors.tooManyArguments)
