b3d43454af7993880968c9ac12052af4923ed8f7
stupid cat <kirkbenm@gmail.com>
2018-08-04 16:05:44 -0600

    fix the invalid color thing

--- src/dcommands/color.js
+++ src/dcommands/color.js
@@ -19,6 +19,9 @@ class ThesearchCommand extends BaseCommand {
             let buffer = await bu.blargbotApi('color', {
                 color: words.slice(1)
             });
+            if (!buffer) {
+                return await bu.send(msg, 'Whoops, one of the things you provided was not a color!');
+            }
             bu.send(msg, undefined, {
                 file: buffer,
                 name: 'color.png'
