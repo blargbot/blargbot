6aa57f2b951209dda62a101ca3e7b37b13a73328
HunteRoi <32441291+HunteRoi@users.noreply.github.com>
2018-08-12 15:11:10 +0200

    Update color.js
    Fixed the module name
    Added error message when
    - the user doesn't provide any color
    - the user provides more than 64 colors (above the buffer capacity)
    Added some info about the command.

--- src/dcommands/color.js
+++ src/dcommands/color.js
@@ -1,28 +1,35 @@
 const BaseCommand = require('../structures/BaseCommand');
 
-class ThesearchCommand extends BaseCommand {
+class ColorCommand extends BaseCommand {
     constructor() {
         super({
             name: 'color',
             category: bu.CommandType.IMAGE,
             usage: 'color [codes]...',
-            info: 'Returns the provided colors'
+            info: 'Returns the provided colors as a PNG file.\n\nYou cannot provide more than 64 entries.\nHex codes must start with an #.'
         });
     }
 
     async execute(msg, words) {
-        var text = 'I use betterdiscord';
-        if (words[1]) text = words.slice(1).join(' ');
-        text = await bu.filterMentions(text);
         bot.sendChannelTyping(msg.channel.id);
-        let buffer = await bu.blargbotApi('color', {
-            color: words.slice(1).map(c => c.replace(/[^\da-f]/g, ''))
-        });
-        bu.send(msg, undefined, {
-            file: buffer,
-            name: 'color.png'
-        });
+        try {
+            const colors = words.slice(1);
+            if (colors.length == 0) return await bu.send(msg, 'Whoops, you did not provide any color!');
+            if (colors.length > 64) return await bu.send(msg, 'Whoops, you provided too many colors!');
+            let buffer = await bu.blargbotApi('color', {
+                color: colors 
+            });
+            if (!buffer) {
+                return await bu.send(msg, 'Whoops, one of the things you provided was not a color!');
+            }
+            bu.send(msg, undefined, {
+                file: buffer,
+                name: 'color.png'
+            });
+        } catch (err) {
+            bu.send(msg, 'Whoops, something went wrong: `' + err.message + '`');
+        }
     }
 }
 
-module.exports = ThesearchCommand;
+module.exports = ColorCommand;
