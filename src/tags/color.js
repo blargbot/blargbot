39267f8e2f3e7ca13fc80817e270f5b3315880f7
byParallax <doyouwanttomakeacakewithme@gmail.com>
2018-08-14 20:29:31 +0200

    Buff 'color' tag

--- src/tags/color.js
+++ src/tags/color.js
@@ -1,29 +1,47 @@
-/*
- * @Author: stupid cat
- * @Date: 2017-05-07 18:30:37
- * @Last Modified by: stupid cat
- * @Last Modified time: 2017-05-07 18:30:37
- *
- * This project uses the AGPLv3 license. Please read the license file before using/adapting any of the code.
- */
-
 const Builder = require('../structures/TagBuilder');
+var Color = require('color');
 
 module.exports =
     Builder.AutoTag('color')
-        .withAlias('colour')
-        .withArgs(a => a.require('color'))
-        .withDesc('Attempts to convert `color` to a hex code. `color` can be a [HTML color](https://www.w3schools.com/colors/colors_names.asp), hex, (r,g,b) or a valid color number.')
+        .withArgs(a => [
+            a.require('text')
+        ]).withDesc('Convert colors. Default output `hex`. It converts all ways between `rgb`, `hsl`, `hsv`, `hwb`, `cmyk`, `ansi16`, `hex` strings, and CSS `keyword`s (will round to closest).')
         .withExample(
-            '{color;pink}',
-            'ffc0cb'
+        '{color;#4286f4;RGB}',
+        '[66,134,244]'
         )
-        .whenArgs(0, Builder.errors.notEnoughArguments)
-        .whenArgs(1, async function (subtag, context, args) {
-            let color = bu.parseColor(args[0]);
-            if (color == null)
-                return Builder.util.error(subtag, context, 'Invalid color');
-            return color.toString(16).padStart(6, '0');
-        })
-        .whenDefault(Builder.errors.tooManyArguments)
+        .whenArgs('0', Builder.errors.notEnoughArguments)
+        .whenArgs('1-2', async function (subtag, context, args) {
+            let arr = await bu.getArray(args[0]);
+            let input = undefined;
+
+            if (arr == null || !Array.isArray(arr.v)) input = args[0];
+            else input = arr.v;
+
+            let method = undefined;
+            if (typeof args[1] === 'undefined') method = 'hex';
+            else method = args[1].toLowerCase();
+
+            let color = undefined;
+            try {
+                color = Color(input);
+            } catch(e) {
+                color = Color('#' + input);
+            }
+
+            if (typeof color === 'undefined') return 'Failed to parse color';
+            if (typeof color[method] !== 'function') return 'Invalid method';
+
+            let converted = color[method]();
+
+            if (typeof converted === 'object') {
+                if (typeof converted.color === 'object') {
+                    return JSON.stringify(converted.color);
+                } else {
+                    return converted.color;
+                }
+            } else {
+                return converted;
+            }
+        }).whenDefault(Builder.errors.tooManyArguments)
         .build();
