3cc71792d97e29f5a5ee6af575405e11e6e54882
stupid cat <kirkbenm@gmail.com>
2018-08-28 19:22:26 -0600

    file support to send

--- src/tags/send.js
+++ src/tags/send.js
@@ -2,7 +2,7 @@
  * @Author: stupid cat
  * @Date: 2017-05-07 18:57:04
  * @Last Modified by: stupid cat
- * @Last Modified time: 2018-03-20 09:38:36
+ * @Last Modified time: 2018-08-28 19:21:33
  *
  * This project uses the AGPLv3 license. Please read the license file before using/adapting any of the code.
  */
@@ -11,16 +11,16 @@ const Builder = require('../structures/TagBuilder');
 
 module.exports =
     Builder.APITag('send')
-        .withArgs(a => [a.require('channel'), a.require([a.optional('message'), a.optional('embed')])])
+        .withArgs(a => [a.require('channel'), a.require([a.optional('message'), a.optional('embed')]), a.optional('file'), a.optional('filename')])
         .withDesc('Sends `message` and `embed` to `channel`, and returns the message ID. `channel` is either an ID or channel mention. ' +
-            'At least one out of `message` and `embed` must be supplied.\n' +
+            'At least one out of `message` and `embed` must be supplied.\nIf `file` is provided, `filename` will default to `file.txt`.\n' +
             'Please note that `embed` is the JSON for an embed object, don\'t put the `{embed}` subtag there, as nothing will show')
         .withExample(
             '{send;#channel;Hello!;{embedbuild;title:You\'re cool}}',
             '1111111111111111111\nIn #channel: Hello!\nEmbed: You\'re cool'
         )
         .whenArgs('0-1', Builder.errors.notEnoughArguments)
-        .whenArgs('2-3', async function (subtag, context, args) {
+        .whenArgs('2-5', async function (subtag, context, args) {
             let channel = bu.parseChannel(args[0], true),
                 message = args[1],
                 embed = bu.parseEmbed(args[1]);
@@ -35,13 +35,19 @@ module.exports =
             if (channel.guild.id != context.guild.id)
                 return Builder.errors.channelNotInGuild(subtag, context);
 
+            let file = args[3], filename = args[4];
+            if (file && !filename) filename = 'file.txt';
+            if (file) {
+                if (!filename) filename = 'file.txt';
+                file = { file, name: filename };
+            }
             try {
                 let sent = await bu.send(channel.id, {
                     content: message,
                     embed: embed,
                     nsfw: context.state.nsfw,
                     disableEveryone: false
-                });
+                }, file);
 
                 context.state.ownedMsgs.push(sent.id);
 
