e3a634418d9d980550ec68a03510ac963822c95f
stupid cat <cat@blargbot.xyz>
2018-09-07 11:17:06 -0600

    restrict args

--- src/tags/json.js
+++ src/tags/json.js
@@ -2,7 +2,7 @@
  * @Author: stupid cat
  * @Date: 2017-05-07 18:50:03
  * @Last Modified by: stupid cat
- * @Last Modified time: 2018-09-07 11:12:11
+ * @Last Modified time: 2018-09-07 11:16:54
  *
  * This project uses the AGPLv3 license. Please read the license file before using/adapting any of the code.
  */
@@ -19,7 +19,7 @@ module.exports =
         '{\n  "key": "value"\n}'
         ).resolveArgs(-1)
         .whenArgs(0, Builder.errors.notEnoughArguments)
-        .whenDefault(async function (subtag, context, args) {
+        .whenArgs(1, async function (subtag, context, args) {
             let raw = args[0].content;
             try {
                 return JSON.parse(raw);
@@ -27,4 +27,5 @@ module.exports =
                 return Builder.errors.invalidJSON(subtag, context);
             }
         })
+        .whenDefault(Builder.errors.tooManyArguments)
         .build();
