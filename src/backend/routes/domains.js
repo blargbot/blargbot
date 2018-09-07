a8f0aca6e9b1a4b05fd4dbde25f64ee6be40c1f8
stupid cat <kirkbenm@gmail.com>
2018-09-07 00:06:09 -0600

    json endpoint

--- src/backend/routes/domains.js
+++ src/backend/routes/domains.js
@@ -2,7 +2,7 @@
  * @Author: stupid cat
  * @Date: 2017-05-07 18:20:10
  * @Last Modified by: stupid cat
- * @Last Modified time: 2018-09-06 17:48:30
+ * @Last Modified time: 2018-09-07 00:06:02
  *
  * This project uses the AGPLv3 license. Please read the license file before using/adapting any of the code.
  */
@@ -10,6 +10,20 @@
 const router = require('express').Router();
 const hbs = require('hbs');
 const moment = require('moment-timezone');
+
+router.get('/json', async function (req, res) {
+    await spawner.recacheDomains();
+    let cache = spawner.domainCache;
+
+    let domains = [];
+    for (const key in cache) {
+        if (cache[key]) domains.push(key);
+    }
+    domains.sort();
+    res.locals.domains = domains;
+
+    res.send(JSON.stringify(domains, null, 2));
+});
 
 router.get('/', async function (req, res) {
     await spawner.recacheDomains();
