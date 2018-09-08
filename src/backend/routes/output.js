dd6fe649b93e0d0650e664b9714a1e1c07302485
stupid cat <kirkbenm@gmail.com>
2018-09-08 13:48:26 -0600

    change output

--- src/backend/routes/output.js
+++ src/backend/routes/output.js
@@ -2,26 +2,40 @@
  * @Author: stupid cat
  * @Date: 2017-05-07 18:19:10
  * @Last Modified by: stupid cat
- * @Last Modified time: 2018-08-07 10:06:20
+ * @Last Modified time: 2018-09-08 13:48:03
  *
  * This project uses the AGPLv3 license. Please read the license file before using/adapting any of the code.
  */
 
 const router = require('express').Router();
 
+async function getOutput(id) {
+    let m = await bu.cclient.execute(`SELECT * FROM message_outputs WHERE id = :id`, {
+        id: id[0]
+    }, { prepare: true });
+
+    return id[1];
+}
+
+router.get('/:id/raw', async function (req, res) {
+    let id = req.params.id.split('.');
+    let output = await getOutput(id[0]);
+    res.set('content-type', 'text/plain');
+    res.send(output.content);
+});
+
 router.get('/:id', async function (req, res) {
     res.locals.user = req.user;
 
     let id = req.params.id.split('.');
 
-    let m = await bu.cclient.execute(`SELECT * FROM message_outputs WHERE id = :id`, {
-        id: id[0]
-    }, { prepare: true });
+    let output = await getOutput(id[0]);
 
-    res.locals.msg = m.rows[0];
+    res.locals.msg = output;
 
     if (id[1]) {
-        res.end(m.rows[0].content, 'utf8');
+        res.set('content-type', 'text/plain');
+        res.end(output.content, 'utf8');
     } else
         res.render('output');
 });
