2dc26c10b7aaf5d600c8385fd557bff5337d347f
stupid cat <cat@blargbot.xyz>
2018-09-13 09:31:27 -0600

    hljs

--- src/backend/routes/output.js
+++ src/backend/routes/output.js
@@ -2,19 +2,23 @@
  * @Author: stupid cat
  * @Date: 2017-05-07 18:19:10
  * @Last Modified by: stupid cat
- * @Last Modified time: 2018-09-13 08:48:14
+ * @Last Modified time: 2018-09-13 09:07:32
  *
  * This project uses the AGPLv3 license. Please read the license file before using/adapting any of the code.
  */
 
 const router = require('express').Router();
 const showdown = require('showdown');
-const converter = new showdown.Converter();
+require('../highlight.js');
+
+const converter = new showdown.Converter({ extensions: ['codehighlight'] });
 converter.setFlavor('github');
 converter.setOption('disableForced4SpacesIndentedSublists', true);
 const xss = require('xss');
 const hbs = require('hbs');
 const moment = require('moment-timezone');
+
+
 
 async function getOutput(id) {
     let m = await bu.cclient.execute(`SELECT * FROM message_outputs WHERE id = :id`, {
@@ -35,6 +39,9 @@ let whiteList = xss.whiteList;
 for (let i = 1; i < 7; i++) {
     whiteList['h' + i].push('id');
 }
+whiteList.span.push('class');
+whiteList.pre.push('class');
+whiteList.code.push('class');
 
 router.get('/:id', async function (req, res) {
     res.locals.user = req.user;
@@ -53,6 +60,7 @@ router.get('/:id', async function (req, res) {
         output.diff = diff.humanize(true);
 
         if (output.content) {
+            console.log(output.content);
             let html = converter.makeHtml(output.content);
             html = xss(html, {
                 whiteList
