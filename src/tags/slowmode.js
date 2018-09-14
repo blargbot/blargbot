4a06a7652745808494c3a904742df918ccba5c3e
stupid cat <cat@blargbot.xyz>
2018-09-14 11:26:17 -0600

    fix slowmode with no channel

--- src/tags/slowmode.js
+++ src/tags/slowmode.js
@@ -2,7 +2,7 @@
  * @Author: stupid cat
  * @Date: 2017-05-21 00:22:32
  * @Last Modified by: stupid cat
- * @Last Modified time: 2018-09-13 19:30:31
+ * @Last Modified time: 2018-09-14 11:26:10
  *
  * This project uses the AGPLv3 license. Please read the license file before using/adapting any of the code.
  */
@@ -15,18 +15,19 @@ module.exports =
         .withArgs(a => [a.optional('channel'), a.optional('time')])
         .withDesc('Enables slowmode for the specified channel. `time` is the amount of seconds required between messages, with a maximum of 120. `channel` is the channel to modify, defaulting to the current one.')
         .withExample(
-            '{slowmode;5}',
-            '(slowmode is enabled at a rate of 1 message per 5 seconds)'
+        '{slowmode;5}',
+        '(slowmode is enabled at a rate of 1 message per 5 seconds)'
         )
         .whenArgs('0-2', async function (subtag, context, args) {
             let channel = bu.parseChannel(args[0], true),
                 time = parseInt(args[1]);
             if (channel === null) {
-                time = parseInt(channel);
+                time = parseInt(args[0]);
                 channel = context.channel;
             }
             if (channel.guild.id != context.guild.id)
                 return Builder.errors.channelNotInGuild(subtag, context);
+            console.log(channel, time);
             if (isNaN(time)) time = 0;
 
             let endpoint = Endpoints.CHANNEL(channel.id);
