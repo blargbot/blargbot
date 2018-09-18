8ecbfd6a57e1390fe8a5e600c3d97048f7ced59d
stupid cat <cat@blargbot.xyz>
2018-09-18 09:47:59 -0600

    set silent to true

--- src/core/discord.js
+++ src/core/discord.js
@@ -2,7 +2,7 @@
  * @Author: stupid cat
  * @Date: 2017-05-07 19:31:12
  * @Last Modified by: stupid cat
- * @Last Modified time: 2018-09-18 09:37:57
+ * @Last Modified time: 2018-09-18 09:47:52
  *
  * This project uses the AGPLv3 license. Please read the license file before using/adapting any of the code.
  */
@@ -161,7 +161,7 @@ class DiscordClient extends Client {
                 tagName: '_interval',
                 author: interval.author,
                 authorizer: interval.authorizer,
-                silent: false
+                silent: true
             });
         }
     }
