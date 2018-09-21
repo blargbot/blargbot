ece59f682bd54dfc6f5d4d084375efad77993bc0
stupid cat <cat@blargbot.xyz>
2018-09-21 08:10:31 -0600

    log on cron

--- src/core/discord.js
+++ src/core/discord.js
@@ -2,7 +2,7 @@
  * @Author: stupid cat
  * @Date: 2017-05-07 19:31:12
  * @Last Modified by: stupid cat
- * @Last Modified time: 2018-09-19 09:31:54
+ * @Last Modified time: 2018-09-21 08:09:30
  *
  * This project uses the AGPLv3 license. Please read the license file before using/adapting any of the code.
  */
@@ -133,6 +133,7 @@ class DiscordClient extends Client {
     }
 
     async autoresponseInterval() {
+        await bu.send('492698595447930881', 'Cron Interval in Cluser: ' + process.env.CLUSTER_ID);
         let guilds = await r.table('guild').getAll(true, { index: 'interval' });
         guilds = guilds.filter(g => this.guilds.get(g.guildid));
         for (const guild of guilds) {
