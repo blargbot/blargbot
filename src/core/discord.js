aa7222fde7023c1da7e5fb2029fb769e614ddf23
stupid cat <kirkbenm@gmail.com>
2018-10-07 18:05:23 -0600

    logs

--- src/core/discord.js
+++ src/core/discord.js
@@ -2,7 +2,7 @@
  * @Author: stupid cat
  * @Date: 2017-05-07 19:31:12
  * @Last Modified by: stupid cat
- * @Last Modified time: 2018-10-07 16:37:36
+ * @Last Modified time: 2018-10-07 18:05:19
  *
  * This project uses the AGPLv3 license. Please read the license file before using/adapting any of the code.
  */
@@ -142,6 +142,7 @@ class DiscordClient extends Client {
         let time = moment();
         let h = (parseInt(time.display('H')) % 2);
         let id = (h ? 0 : 4) + (parseInt(time.format('m')) / 15);
+        console.info('Switching avatar to #', id);
         await bot.editSelf({
             avatar: bu.avatars[id]
         });
