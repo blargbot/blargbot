985d7a64ab84e396461cb5e4943be60eb419ffa8
stupid cat <kirkbenm@gmail.com>
2018-10-12 14:14:23 -0600

    non-expiring outputs

--- src/backend/routes/output.js
+++ src/backend/routes/output.js
@@ -2,7 +2,7 @@
  * @Author: stupid cat
  * @Date: 2017-05-07 18:19:10
  * @Last Modified by: stupid cat
- * @Last Modified time: 2018-09-13 09:07:32
+ * @Last Modified time: 2018-10-12 14:14:12
  *
  * This project uses the AGPLv3 license. Please read the license file before using/adapting any of the code.
  */
@@ -21,7 +21,7 @@ const moment = require('moment-timezone');
 
 
 async function getOutput(id) {
-    let m = await bu.cclient.execute(`SELECT * FROM message_outputs WHERE id = :id`, {
+    let m = await bu.cclient.execute(`SELECT id, content, embeds, channelid, TTL(content) as expiry FROM message_outputs WHERE id = :id`, {
         id
     }, { prepare: true });
 
@@ -54,11 +54,15 @@ router.get('/:id', async function (req, res) {
         res.set('content-type', 'text/plain');
         res.end(output.content, 'utf8');
     } else {
-        let date = moment(bu.unmakeSnowflake(output.id)).add(7, 'days');
-        let diff = moment.duration(-1 * (moment() - date));
-        output.expiry = date.tz('etc/utc').format('MMMM Do, YYYY')
-        output.diff = diff.humanize(true);
-
+        if (output.expiry > 0) {
+            let date = moment(bu.unmakeSnowflake(output.id)).add(output.expiry, 'seconds');
+            let diff = moment.duration(-1 * (moment() - date));
+            output.expiry = date.tz('etc/utc').format('MMMM Do, YYYY');
+            output.diff = diff.humanize(true);
+        } else {
+            output.expiry = 'never';
+            output.diff = 'never';
+        }
         if (output.content) {
             console.log(output.content);
             let html = converter.makeHtml(output.content);
@@ -71,6 +75,26 @@ router.get('/:id', async function (req, res) {
         res.locals.msg = output;
         res.render('output');
     }
+});
+
+router.get('/:id/perm', async function (req, res) {
+    res.locals.user = req.user;
+
+    if (req.user.id !== '103347843934212096') {
+        res.status(401);
+        res.send('You do not have permission to do this.');
+        return;
+    }
+
+    let id = req.params.id.split('.');
+
+    let output = await getOutput(id[0]);
+
+    let m = await bu.cclient.execute(`UPDATE message_outputs USING TTL 0 WHERE id = :id`, {
+        id
+    }, { prepare: true });
+
+    res.send('ok');
 });
 
 module.exports = router;
