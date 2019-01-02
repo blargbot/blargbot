a429354c8f39845ebf358c257bad61f00390b78d
stupid cat <kirkbenm@gmail.com>
2019-01-02 12:26:42 -0700

    some role modification

--- src/tags/rolesetmentionable.js
+++ src/tags/rolesetmentionable.js
@@ -2,7 +2,7 @@
  * @Author: stupid cat
  * @Date: 2017-05-21 00:22:32
  * @Last Modified by: stupid cat
- * @Last Modified time: 2018-05-16 10:18:41
+ * @Last Modified time: 2019-01-02 12:25:07
  *
  * This project uses the AGPLv3 license. Please read the license file before using/adapting any of the code.
  */
@@ -37,7 +37,9 @@ module.exports =
                     return Builder.util.error(subtag, context, 'Role above author');
 
                 try {
-                    await role.edit({ mentionable });
+                    await role.edit({ mentionable },
+                        `Role edit by ${context.user.username}#${context.user.discriminator}`
+                        + (context.scope.reason ? ': ' + context.scope.reason : ''));
                     return;
                 } catch (err) {
                     if (!quiet)
