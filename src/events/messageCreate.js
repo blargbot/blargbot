8be67fe223f607e6840ec12161d01eb5989ebc39
stupid cat <kirkbenm@gmail.com>
2019-07-29 17:55:31 -0600

    add guild to shallow copy

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -2,7 +2,7 @@
  * @Author: stupid cat
  * @Date: 2017-05-07 18:22:24
  * @Last Modified by: stupid cat
- * @Last Modified time: 2019-07-29 17:54:37
+ * @Last Modified time: 2019-07-29 17:55:23
  *
  * This project uses the AGPLv3 license. Please read the license file before using/adapting any of the code.
  */
@@ -457,7 +457,8 @@ async function handleAutoresponse(msg, storedGuild, everything = false) {
     if (storedGuild && storedGuild.autoresponse) {
         let ars = storedGuild.autoresponse;
         let m = {
-            ...msg
+            ...msg,
+            guild: msg.guild
         };
 
         if (everything && ars.everything && storedGuild.ccommands[ars.everything.executes]) {
