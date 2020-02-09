0a50a1b7dbb7a9ce6c3b9a736bc45e7591504abd
stupid cat <cat@blargbot.xyz>
2020-02-09 00:52:48 -0700

    revert httpsInterceptor

--- src/core/httpsInterceptor.js
+++ src/core/httpsInterceptor.js
@@ -1,11 +1,11 @@
 const https = require('https');
 
-const originalRequest = https.request;
-https.request = function (...args) {
-  const data = args[0];
-  if (data && data.host === 'discordapp.com') {
-    const route = data.path.replace(/\?.+$/, '').replace(/(\d{2,}|[^\/]{30,})/g, '_x').replace(/reactions\/.+$/i, 'reactions/_r');
-    bot.sender.send('httpsMetric', { method: data.method, route });
-  }
-  return originalRequest(...args);
-};
+// const originalRequest = https.request;
+// https.request = function (...args) {
+//   const data = args[0];
+//   if (data && data.host === 'discordapp.com') {
+//     const route = data.path.replace(/\?.+$/, '').replace(/(\d{2,}|[^\/]{30,})/g, '_x').replace(/reactions\/.+$/i, 'reactions/_r');
+//     bot.sender.send('httpsMetric', { method: data.method, route });
+//   }
+//   return originalRequest(...args);
+// };
