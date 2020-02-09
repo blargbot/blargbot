c14c3b2be288d1341c85c790c49ff2e3b9df8bd1
stupid cat <cat@blargbot.xyz>
2020-02-09 00:03:37 -0700

    add reaction filter

--- src/core/httpsInterceptor.js
+++ src/core/httpsInterceptor.js
@@ -4,7 +4,7 @@ const originalRequest = https.request;
 https.request = function (...args) {
   const data = args[0];
   if (data && data.host === 'discordapp.com') {
-    const route = data.path.replace(/\?.+$/, '').replace(/(\d{2,}|[^\/]{30,})/g, '_x');
+    const route = data.path.replace(/\?.+$/, '').replace(/(\d{2,}|[^\/]{30,})/g, '_x').replace(/reactions\/.+$/i, 'reactions/_r');
     bot.sender.send('httpsMetric', { method: data.method, route });
   }
   return originalRequest(...args);
