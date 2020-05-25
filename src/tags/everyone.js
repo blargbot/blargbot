c02333f752318702d975f12e7d6d8ad86987ab0e
RagingLink <brianbert03@gmail.com>
2020-05-25 22:22:38 +0200

    Customization of everyone ping
    Extended the customization of everyone pings via allowedMentions

---
+++ src/tags/everyone.js
@@ -0,0 +1,30 @@
+/*
+ * @Author: stupid cat
+ * @Date: 2017-05-07 18:38:55
+ * @Last Modified by: stupid cat
+ * @Last Modified time: 2017-10-19 18:12:29
+ *
+ * This project uses the AGPLv3 license. Please read the license file before using/adapting any of the code.
+ */
+
+const Builder = require('../structures/TagBuilder');
+
+module.exports =
+    Builder.BotTag('everyone')
+        .withArgs(a => [a.optional('enabled')])
+        .withDesc(
+            'Returns the mention of `@everyone`.\n' +
+            'If `enabled` is `false` it will return a non-pinging `@everyone`, defaults to `true`'
+        )
+        .withExample(
+            '{everyone}',
+            '@everyone'
+        )
+        .whenArgs('0-1', async function (subtag, context, args) {
+            let enabled = bu.parseBoolean(args[0], true);
+            context.state.allowedMentions.everybody = enabled;
+            return "@everyone";
+        })
+        .whenDefault(Builder.errors.tooManyArguments)
+        .build();
+        
