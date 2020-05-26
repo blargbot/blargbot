b9652874106f95f739c568d47c357dd610a37ce3
RagingLink <brianbert03@gmail.com>
2020-05-26 11:41:45 +0200

    Changed name to everyonemention and added alias
    Added alias `everyone` and changed name to `everyonemention`

--- src/tags/everyone.js
+++ src/tags/everyone.js
@@ -1,30 +1,31 @@
-/*
- * @Author: stupid cat
- * @Date: 2017-05-07 18:38:55
- * @Last Modified by: stupid cat
- * @Last Modified time: 2017-10-19 18:12:29
+/**
+ * @Author: RagingLink 
+ * @Date: 2020-05-26 11:32:51
+ * @Last Modified by: RagingLink
+ * @Last Modified time: 2020-05-26 11:40:57
  *
  * This project uses the AGPLv3 license. Please read the license file before using/adapting any of the code.
- */
+*/
 
 const Builder = require('../structures/TagBuilder');
 
 module.exports =
-    Builder.APITag('everyone')
+    Builder.APITag('everyonemention')
+        .withAlias('everyone')
         .withArgs(a => [a.optional('enabled')])
         .withDesc(
             'Returns the mention of `@everyone`.\n' +
-            'If `enabled` is `false` it will return a non-pinging `@everyone`, defaults to `true`'
+            'If `enabled` is `false` it will return a non-pinging `@everyone`, defaults to `true`.'
         )
         .withExample(
-            '{everyone}',
+            '{everyonemention}',
             '@everyone'
         )
         .whenArgs('0-1', async function (subtag, context, args) {
             let enabled = bu.parseBoolean(args[0], true);
             context.state.allowedMentions.everybody = enabled;
+
             return "@everyone";
         })
         .whenDefault(Builder.errors.tooManyArguments)
         .build();
-        
