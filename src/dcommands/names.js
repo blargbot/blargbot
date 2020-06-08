9408c3ed1b76e05f7785fad17175804c252684e6
RagingLink <brianbert03@gmail.com>
2020-06-08 15:07:50 +0200

    Added flag to remove usernames
    Added flag so users are free to remove their usernames from the db

--- src/dcommands/names.js
+++ src/dcommands/names.js
@@ -8,18 +8,40 @@ class NamesCommand extends BaseCommand {
             category: bu.CommandType.GENERAL,
             usage: 'names [user] [flags]',
             info: 'Returns the names that I\'ve seen the specified user have in the past 30 days.',
-            flags: [{ flag: 'a', word: 'all', desc: 'Gets all the names.' },
-            {
-                flag: 'v',
-                word: 'verbose',
-                desc: 'Gets more information about the retrieved names.'
-            }]
+            flags: [
+                {
+                    flag: 'a',
+                    word: 'all',
+                    desc: 'Gets all the names.'
+                },
+                {
+                    flag: 'v',
+                    word: 'verbose',
+                    desc: 'Gets more information about the retrieved names.'
+                },
+                {
+                    flag: 'r',
+                    word: 'remove',
+                    desc: 'Removes all your usernames from the database.'
+                }
+            ]
         });
     }
 
     async execute(msg, words, text) {
         let input = bu.parseInput(this.flags, words);
         let user;
+        if (input.r) {
+            user = msg.author;
+            let storedUser = bu.getCachedUser(user.id);
+            let updatedUser = { usernames: [] };
+            if (storedUser.usernames.length > 0 ) {
+                await r.table('user').get(user.id).update(updatedUser).run(); 
+                return bu.send(msg, `Succesfully removed ${storedUser.usernames.length} username${storedUser.usernames.length > 1 ? 's' : ''}.`);
+            } else {
+                return bu.send(msg, 'No usernames could be removed!')
+            }
+        }
         if (input.undefined.length == 0) {
             user = msg.author;
         } else {
