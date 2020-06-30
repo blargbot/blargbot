459d18c415cb7987ac613b8534a65d1aed2c7572
RagingLink <brianbert03@gmail.com>
2020-06-30 21:59:55 +0200

    Created tag to escape bbtag code

---
+++ src/tags/escapebbtag.js
@@ -0,0 +1,27 @@
+/**
+ * @Author: RagingLink 
+ * @Date: 2020-06-25 12:25:05
+ * @Last Modified by: RagingLink
+ * @Last Modified time: 2020-06-30 21:59:10
+ *
+ * This project uses the AGPLv3 license. Please read the license file before using/adapting any of the code.
+ */
+
+const Builder = require('../structures/TagBuilder');
+
+module.exports =
+    Builder.BotTag('escapebbtag')
+        .withArgs(a => [a.require('code')])
+        .withAlias('escape')
+        .withDesc('Returns `code` without executing any BBtag inside it.\n' +
+            'This effectively returns the characters `{`,`}` and `;` as is, without the use of `{rb}`, `{lb}` and `{semi}`.')
+        .withExample(
+            '{escapebbtag;{set;~index;1}}',
+            '{set;~index;1}'
+        )
+        .resolveArgs(-1)
+        .whenArgs(0, Builder.errors.notEnoughArguments)
+        .whenDefault(async (subtag, context, args) => {
+            return args.map(arg => arg.content).join(';');
+        })
+        .build();
