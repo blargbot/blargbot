604d6c46cd0403d1323cadae335328e4a1747c2f
RagingLink <nicelink.brian@gmail.com>
2021-02-02 00:58:47 +0100

    New flagsarray subtag (#232)
    * Create flags.js
    
    * Fixed arguments 
    
    fixed dumb .withArgs mistake
    
    * Renamed to flagsarray

---
+++ src/tags/flagsarray.js
@@ -0,0 +1,24 @@
+/*
+ * @Author: RagingLink
+ * @Date: 2021-28-01 11:37:28
+ * @Last Modified by: RagingLink
+ * @Last Modified time: 2021-28-01 11:37:28
+ *
+ * This project uses the AGPLv3 license. Please read the license file before using/adapting any of the code.
+ */
+
+const Builder = require('../structures/TagBuilder');
+
+module.exports =
+    Builder.BotTag('flagsarray')
+        .withDesc('Returns an array of all flags provided.')
+        .withExample(
+            '{flagsarray}',
+            'Hello -dc world',
+            '["_","d","c"]'
+        )
+        .whenArgs(0, async function (_, context, __) {
+            return Object.keys(context.flaggedInput);
+        })
+        .whenDefault(Builder.errors.tooManyArguments)
+        .build();
