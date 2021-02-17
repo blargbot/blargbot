d978f871d72e2e391941feb601cca9ee68a06636
Danny May <dannyjmay97@gmail.com>
2021-02-17 20:47:47 +0000

    Fix build errors

--- src/core/bbtag/limits/EverythingAutoResponseLimit.ts
+++ src/core/bbtag/limits/EverythingAutoResponseLimit.ts
@@ -44,7 +44,7 @@ export class EverythingAutoResponseLimit extends BaseRuntimeLimit {
             .addRules('timer', DisabledRule.instance)
             .addRules('sleep', {
                 async check(context, subtag) {
-                    if (parse.int(await context.execute(subtag.args[0])) > 5000)
+                    if (parse.int(await context.eval(subtag.args[0])) > 5000)
                         subtag.args[0] = ['5000']; // Soft limit for duration
                     return true;
                 },
