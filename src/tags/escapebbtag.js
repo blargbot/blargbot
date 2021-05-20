269e9afd0518fbe8c496db04c0dc9176acd3af96
RagingLink <nicelink.brian@gmail.com>
2021-05-21 00:05:50 +0200

    Include caveat

--- src/tags/escapebbtag.js
+++ src/tags/escapebbtag.js
@@ -2,7 +2,7 @@
  * @Author: RagingLink 
  * @Date: 2020-06-25 12:25:05
  * @Last Modified by: RagingLink
- * @Last Modified time: 2020-06-30 21:59:10
+ * @Last Modified time: 2021-05-21 00:05:34
  *
  * This project uses the AGPLv3 license. Please read the license file before using/adapting any of the code.
  */
@@ -14,7 +14,8 @@ module.exports =
         .withArgs(a => [a.require('code')])
         .withAlias('escape')
         .withDesc('Returns `code` without executing any BBtag inside it.\n' +
-            'This effectively returns the characters `{`,`}` and `;` as is, without the use of `{rb}`, `{lb}` and `{semi}`.')
+            'This effectively returns the characters `{`,`}` and `;` as is, without the use of `{rb}`, `{lb}` and `{semi}`.\n' +
+            '**NOTE**: Brackets inside `code` must come in pairs. A { *has to be* followed by a } somewhere and a } *has to have* a { before it')
         .withExample(
             '{escapebbtag;{set;~index;1}}',
             '{set;~index;1}'
