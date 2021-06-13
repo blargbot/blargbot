9b68aedfeae3c852d2fe30793ca2522826bc7934
RagingLink <nicelink.brian@gmail.com>
2021-06-13 22:10:03 +0200

    What was I even trying to do here?

---
+++ src/tags/concat.ts
@@ -0,0 +1,28 @@
+import { Cluster } from '../cluster';
+import { BaseSubtag } from '../core/bbtag';
+import { SubtagType, bbtagUtil } from '../utils';
+
+export class ConcatSubtag extends BaseSubtag {
+    public constructor(cluster: Cluster) {
+        super(cluster, {
+            name: 'concat',
+            category: SubtagType.ARRAY,
+            definition: [
+                {
+                    args: ['arrays+'],
+                    description:
+                        'Takes `arrays` and joins them together to form a single array.',
+                    exampleCode: '{concat;["this", "is"];["an", "array"]}',
+                    exampleOut: '["this","is","an","array"]',
+                    execute: (_, args) => this.concatArrays(args.map((arg) => arg.value))
+                }
+            ]
+        });
+    }
+
+    public concatArrays(arrays: string[]): string {
+        const parsedArray = arrays.map((array) => JSON.parse(array));
+        const flattenedArray = bbtagUtil.tagArray.flattenArray(parsedArray);
+        return JSON.stringify(flattenedArray);
+    }
+}
