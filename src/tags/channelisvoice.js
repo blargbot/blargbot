b53a409e0fc23f254cb36797f837b541d4cba14a
Brian <nicelink.brian@gmail.com>
2021-06-19 19:18:46 +0200

    Respect suppresslookup for channel lookups (#262)

--- src/tags/channelisvoice.js
+++ src/tags/channelisvoice.js
@@ -2,7 +2,7 @@
  * @Author: zoomah
  * @Date: 2018-07-10 7:08:15
  * @Last Modified by: RagingLink
- * @Last Modified time: 2021-06-13 15:01:05
+ * @Last Modified time: 2021-06-19 17:49:53
  *
  * This project uses the AGPLv3 license. Please read the license file before using/adapting any of the code.
  */
@@ -21,7 +21,7 @@ module.exports =
         .whenArgs(0, (_, context) => context.channel.type == 2)
         .whenArgs('1-2', async function (subtag, context, args) {
             let quiet = bu.isBoolean(context.scope.quiet) ? context.scope.quiet : !!args[1];
-            let channel = await Builder.util.parseChannel(context, args[0], { quiet });
+            let channel = await Builder.util.parseChannel(context, args[0], { quiet, suppress: context.scope.suppressLookup });
 
             if (!channel)
                 return quiet ? false : Builder.errors.noChannelFound(subtag, context);
