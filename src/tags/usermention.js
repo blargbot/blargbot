bb0d1d41409a9dfb873c323ffe2f767327a51ae3
Brian <nicelink.brian@gmail.com>
2021-07-03 19:39:45 +0200

    Parameter that toggles pings added (#269)

--- src/tags/usermention.js
+++ src/tags/usermention.js
@@ -1,8 +1,8 @@
 /*
  * @Author: stupid cat
  * @Date: 2017-05-07 19:20:35
- * @Last Modified by: stupid cat
- * @Last Modified time: 2018-05-16 10:01:47
+ * @Last Modified by: RagingLink
+ * @Last Modified time: 2021-07-03 19:13:01
  *
  * This project uses the AGPLv3 license. Please read the license file before using/adapting any of the code.
  */
@@ -11,17 +11,21 @@ const Builder = require('../structures/TagBuilder');
 
 module.exports =
     Builder.APITag('usermention')
-        .withArgs(a => [a.optional('user'), a.optional('quiet')])
+        .withArgs(a => [a.optional('user'), a.optional('quiet'), a.optional('noPing')])
         .withDesc('Mentions `user`. `user` defaults to the user who executed the containing tag. ' +
-            'If `quiet` is specified, if `user` can\'t be found it will simply return nothing.')
+            'If `quiet` is specified (can be any value), if `user` can\'t be found it will simply return nothing. ' +
+            'If `noPing` is `true` (must be a boolean), user won\'t be pinged but the mention will still be displayed, if it is `false` the mention will ping.')
         .withExample(
             'Hello, {usermention}!',
             'Hello, @user!'
         )
-        .whenArgs('0-2', async function (subtag, context, args) {
+        .whenArgs('0-3', async function (subtag, context, args) {
             let quiet = bu.isBoolean(context.scope.quiet) ? context.scope.quiet : !!args[1],
                 user = context.user;
-
+            const noPing = bu.parseBoolean(args[2]);
+            if (args[2] && typeof noPing !== 'boolean') {
+                return Builder.errors.notABoolean(subtag, context);
+            }
             if (args[0])
                 user = await context.getUser(args[0], {
                     quiet, suppress: context.scope.suppressLookup,
@@ -29,7 +33,7 @@ module.exports =
                 });
 
             if (user != null) {
-                if (!context.state.allowedMentions.users.includes(user.id)) {
+                if (!noPing && !context.state.allowedMentions.users.includes(user.id)) {
                     context.state.allowedMentions.users.push(user.id);
                 }
                 return user.mention;
