31123a63d2bd0de863d945ee878603de4b6309b4
danny-may <dannyjmay97@gmail.com>
2021-08-14 00:06:09 +0100

    Add truth command

---
+++ src/workers/cluster/dcommands/truth.ts
@@ -0,0 +1,22 @@
+import { BaseGlobalImageCommand, CommandContext } from '@cluster/command';
+import { ImageResult } from '@image/types';
+
+export class TruthCommand extends BaseGlobalImageCommand {
+    public constructor() {
+        super({
+            name: 'truth',
+            definitions: [
+                {
+                    parameters: '{text+}',
+                    description: 'Shows everyone what is written in the Scroll of Truth.',
+                    execute: (ctx, [text]) => this.render(ctx, text)
+                }
+            ]
+        });
+    }
+
+    public async render(context: CommandContext, text: string): Promise<ImageResult | string> {
+        text = await context.util.resolveTags(context, text);
+        return await this.renderImage(context, 'truth', { text });
+    }
+}
