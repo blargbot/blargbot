80f18e6c2f2175b42385010c05fd5802e7f1ccdb
Brian <nicelink.brian@gmail.com>
2021-08-14 00:12:03 +0200

    Fixed inject relying on removed code

--- src/workers/cluster/subtags/inject.ts
+++ src/workers/cluster/subtags/inject.ts
@@ -1,5 +1,5 @@
 import { BaseSubtag } from '@cluster/bbtag';
-import { bbtagUtil, SubtagType } from '@cluster/utils';
+import { SubtagType } from '@cluster/utils';
 
 export class InjectSubtag extends BaseSubtag {
     public constructor() {
@@ -10,10 +10,12 @@ export class InjectSubtag extends BaseSubtag {
                 {
                     parameters: ['code'],
                     description: 'Executes any arbitrary BBTag that is within `code` and returns the result. Useful for making dynamic code, or as a testing tool (`{inject;{args}}`)',
-                    exampleCode: 'Random Number: {inject;{lb}randint{semi}1{semi}4{lb}}',
+                    exampleCode: 'Random Number: {inject;{lb}randint{semi}1{semi}4{rb}}',
                     exampleOut: 'Random Number: 3',
-                    execute: async (context, [{value: code}], subtag) => {
-                        return await bbtagUtil.exec(context, subtag, code, '');
+                    execute: async (context, [{value: code}]) => {
+                        const result = await context.engine.execute(code, context.makeChild());
+                        context.errors.push(...result.errors);
+                        return result.content;
                     }
                 }
             ]
