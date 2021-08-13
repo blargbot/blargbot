55d6c7069d40c8145194668fae145844befd3b3c
danny-may <dannyjmay97@gmail.com>
2021-08-14 00:20:29 +0100

    Add shit command

---
+++ src/workers/cluster/dcommands/shit.ts
@@ -0,0 +1,25 @@
+import { BaseGlobalImageCommand, CommandContext } from '@cluster/command';
+import { ImageResult } from '@image/types';
+
+export class ShitCommand extends BaseGlobalImageCommand {
+    public constructor() {
+        super({
+            name: 'shit',
+            definitions: [
+                {
+                    parameters: '{text+=Your favourite anime}',
+                    description: 'Tells everyone what\'s shit.',
+                    execute: (ctx, [text], flags) => this.render(ctx, text, flags.p !== undefined)
+                }
+            ],
+            flags: [
+                { flag: 'p', word: 'plural', description: 'Whether or not the text is plural (use ARE instead of IS).' }
+            ]
+        });
+    }
+
+    public async render(context: CommandContext, text: string, plural: boolean): Promise<ImageResult | string> {
+        text = await context.util.resolveTags(context, text);
+        return await this.renderImage(context, 'shit', { text, plural });
+    }
+}
