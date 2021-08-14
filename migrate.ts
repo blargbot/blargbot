a43eb0fb6ed96e29ff5a9fe09832da15173ee384
danny-may <dannyjmay97@gmail.com>
2021-08-14 12:26:47 +0100

    Add changelog command

---
+++ migrate.ts
@@ -0,0 +1,75 @@
+import 'module-alias/register';
+
+import config from '@config';
+import { Database } from '@core/database';
+import { createLogger, Logger } from '@core/Logger';
+import { guard } from '@core/utils';
+import { AllChannels, Client as Discord } from 'discord.js';
+
+void (async function () {
+    const logger = createLogger(config, 'MIGRATE');
+    // This is for migrating from the js blarg to the ts blarg
+    const discord = new Discord({
+        intents: [
+            'GUILD_WEBHOOKS',
+            'GUILDS',
+            'GUILD_INTEGRATIONS',
+            'GUILD_MESSAGES'
+        ],
+        shardCount: 100,
+        shards: [0]
+    });
+    const database = new Database({
+        discord: discord,
+        cassandra: config.cassandra,
+        logger: logger,
+        postgres: config.postgres,
+        rethinkDb: config.rethink
+    });
+
+    await Promise.all([
+        discord.login(config.discord.token),
+        database.connect()
+    ]);
+
+    await Promise.allSettled([
+        migrateChangelog(discord, database, logger)
+    ]);
+
+    process.exit();
+})();
+
+async function migrateChangelog(discord: Discord, database: Database, logger: Logger): Promise<void> {
+    const changelogs = await database.vars.get('changelog');
+    await discord.guilds.fetch(config.discord.guilds.home);
+    const changelogChannel = <AllChannels | null>await discord.channels.fetch(config.discord.channels.changelog);
+    if (changelogChannel === null || changelogChannel.type !== 'GUILD_NEWS' || !('addFollower' in changelogChannel)) {
+        logger.error('[migrateChangelog] Cannot locate changelog news channel');
+        return;
+    }
+
+    const unmigrated = { ...changelogs?.guilds ?? {} };
+    for (const [guildId, channelId] of Object.entries(unmigrated)) {
+        try {
+            const channel = await discord.channels.fetch(channelId, { allowUnknownGuild: true });
+            if (channel !== null && guard.isTextableChannel(channel)) {
+                try {
+                    await changelogChannel.addFollower(channel.id, 'Blargbot changelogs moved to news channels');
+                    await channel.send('Hi! Ive recently received an update to how changelogs work. This channel was subscribed, so I have migrated it to the new method!');
+                } catch (ex: unknown) {
+                    logger.error('[migrateChangelog] Guild:', guildId, 'Channel:', channelId, ex);
+                    await channel.send('Hi! Ive recently received an update to how changelogs work. This channel was subscribed, but I wasnt able to update it to the new method. Please run `b!changelog subscribe` to fix this!');
+                }
+            }
+            delete unmigrated[guildId];
+        } catch (ex: unknown) {
+            logger.error('[migrateChangelog] Guild:', guildId, 'Channel:', channelId, ex);
+        }
+    }
+
+    await database.vars.set({ varname: 'changelog', guilds: {} });
+
+    const failed = Object.entries(unmigrated);
+    const success = Object.keys(changelogs?.guilds ?? {}).length - failed.length;
+    logger.info('[migrateChangelog] Complete.', success, 'channels updated.', failed.length, 'channels failed:', unmigrated);
+}
---
+++ src/workers/cluster/dcommands/admin/changelog.ts
@@ -0,0 +1,59 @@
+import { BaseGuildCommand } from '@cluster/command';
+import { GuildCommandContext } from '@cluster/types';
+import { CommandType } from '@cluster/utils';
+import { guard, humanize } from '@core/utils';
+import { Webhook } from 'discord.js';
+
+export class ChangelogCommand extends BaseGuildCommand {
+    public constructor() {
+        super({
+            name: 'changelog',
+            category: CommandType.ADMIN,
+            definitions: [
+                {
+                    parameters: 'subscribe|sub|track',
+                    description: 'Subscribes this channel to my changelog updates. I require the `manage webhooks` permission for this.',
+                    execute: (ctx) => this.addFollower(ctx)
+                },
+                {
+                    parameters: 'unsubscribe|unsub|untrack',
+                    description: 'Unsubscribes this channel from my changelog updates. I require the `manage webhooks` permission for this.',
+                    execute: (ctx) => this.removeFollower(ctx)
+                }
+            ]
+        });
+    }
+
+    public async addFollower(context: GuildCommandContext): Promise<string> {
+        const current = await this.getCurrentSubscription(context);
+        if (typeof current !== 'undefined')
+            return this.info('This channel is already subscribed to my changelog updates!');
+
+        const changelogChannel = await context.util.getChannelById(context.config.discord.channels.changelog);
+        if (changelogChannel === undefined || !guard.isGuildChannel(changelogChannel) || changelogChannel.type !== 'GUILD_NEWS')
+            return this.error('It looks like I cant find the where to get changelog messages from! Please try again later.');
+
+        await changelogChannel.addFollower(context.channel, `${humanize.fullName(context.author)} subscribed channel to changelog updates`);
+        return this.success('This channel will now get my changelog updates!');
+    }
+
+    public async removeFollower(context: GuildCommandContext): Promise<string> {
+        const current = await this.getCurrentSubscription(context);
+        if (typeof current !== 'object')
+            return current ?? this.info('This channel is not subscribed to my changelog updates!');
+
+        await current.delete(`${humanize.fullName(context.author)} unsubscribed channel to changelog updates`);
+        return this.success('This channel will no longer get my changelog updates!');
+    }
+
+    private async getCurrentSubscription(context: GuildCommandContext): Promise<Webhook | string | undefined> {
+        if (context.channel.guild.me?.permissions.has('MANAGE_WEBHOOKS') !== true)
+            return this.error('I need the manage webhooks permission to subscribe this channel to changelogs!');
+
+        const webhooks = await context.channel.guild.fetchWebhooks();
+        return webhooks.find(hook =>
+            hook.sourceChannel?.id === context.config.discord.channels.changelog
+            && hook.channelId === context.channel.id
+        );
+    }
+}
