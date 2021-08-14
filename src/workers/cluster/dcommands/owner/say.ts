d29560942d949beb1cf62413dcb7aed37d316b0d
danny-may <dannyjmay97@gmail.com>
2021-08-14 18:40:09 +0100

    Add say command

---
+++ src/workers/cluster/dcommands/owner/say.ts
@@ -0,0 +1,32 @@
+import { BaseGlobalCommand, CommandContext } from '@cluster/command';
+import { CommandType } from '@cluster/utils';
+import { guard } from '@core/utils';
+import { AllChannels } from 'discord.js';
+
+export class SayCommand extends BaseGlobalCommand {
+    public constructor() {
+        super({
+            name: 'say',
+            category: CommandType.OWNER,
+            definitions: [
+                {
+                    parameters: '{channel:channel} {text+}',
+                    description: 'Sends `text` to the given channel',
+                    execute: (ctx, [channel, text]) => this.say(ctx, channel, text)
+                },
+                {
+                    parameters: 'here {text+}',
+                    description: 'Sends `text` to this channel',
+                    execute: (ctx, [text]) => this.say(ctx, ctx.channel, text)
+                }
+            ]
+        });
+    }
+
+    public async say(context: CommandContext, channel: AllChannels, text: string): Promise<void> {
+        if (!guard.isTextableChannel(channel))
+            await context.reply(`You cant send messages to ${channel.toString()}`);
+
+        await context.util.send(channel.id, text);
+    }
+}
