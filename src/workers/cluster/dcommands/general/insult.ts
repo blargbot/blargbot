46594931d7c040aea401df2e2034305721927250
danny-may <dannyjmay97@gmail.com>
2021-08-15 20:50:36 +0100

    Add insult command

---
+++ src/workers/cluster/dcommands/general/insult.ts
@@ -0,0 +1,131 @@
+import { BaseGlobalCommand } from '@cluster/command';
+import { CommandType, randChoose } from '@cluster/utils';
+
+export class InsultCommand extends BaseGlobalCommand {
+    public constructor() {
+        super({
+            name: 'insult',
+            category: CommandType.GENERAL,
+            definitions: [
+                {
+                    parameters: '{name+}',
+                    description: 'Generates a random insult directed at the name supplied.',
+                    execute: (_, [name]) => this.insult(`${<string>name}'s`)
+                },
+                {
+                    parameters: '',
+                    description: 'Generates a random insult.',
+                    execute: () => this.insult('Your')
+                }
+            ]
+        });
+    }
+
+    public insult(who: string): string {
+        return `${who} ${randChoose(nouns)} ${randChoose(verbs)} ${randChoose(adjectives)}`;
+    }
+}
+
+const verbs = [
+    'smells like',
+    'looks like',
+    'is',
+    'sounds like',
+    'appears to be',
+    'wants to be',
+    'looks just like',
+    'smells oddly similar to',
+    'is jealous of',
+    'is as stupid as',
+    'laughs like'
+];
+const nouns = [
+    'mother',
+    'mom',
+    'father',
+    'dad',
+    'goat',
+    'cheese',
+    'dick',
+    'boob',
+    'eye',
+    'mouth',
+    'nose',
+    'ear',
+    'sister',
+    'sis',
+    'brother',
+    'bro',
+    'seagull',
+    'tea',
+    'mother-in-law',
+    'rabbit',
+    'dog',
+    'cat',
+    'left foot',
+    'body',
+    'brain',
+    'face',
+    'favourite thing'
+];
+const adjectives = [
+    'a piece of cheese',
+    'a smelly fish',
+    'jam',
+    'tea',
+    'a skunk',
+    'a fart',
+    'a piece of toast',
+    'my mom',
+    'your mom',
+    'my dad',
+    'your dad',
+    'my sister',
+    'your sister',
+    'my brother',
+    'your brother',
+    'my cat',
+    'my dog',
+    'my lizard',
+    'my seagull',
+    'gross',
+    'farts',
+    'ugly',
+    'Captain America',
+    'javascript',
+    'C#',
+    'LUA',
+    'python3.5',
+    'a furry',
+    'an anthropomorphic horse',
+    'a tentacle monster',
+    'fuck',
+    'meow',
+    'mississippi',
+    'the entire UK',
+    'Japan',
+    'anime',
+    'dickgirls',
+    'a really stupid cat',
+    'a sentient robot',
+    'teaching a robot to love',
+    'anime girls with really large boobs who want to eat all of your cream',
+    'salty',
+    'smegma',
+    'mouldy cheese',
+    'obesity',
+    'Donald Trump',
+    'stupid people',
+    'crabcakes',
+    'firepoles',
+    'blue waffle',
+    'a really bad random insult generators',
+    'a terrible AI',
+    'cleverbot',
+    'b1nzy',
+    'a drunken goblin',
+    'poorly censored porn',
+    'an egg left in the sun for too long',
+    '#BREXIT',
+    'leaving the EU'
+];
