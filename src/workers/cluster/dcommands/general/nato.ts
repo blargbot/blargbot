9bc08e1b538bedb6cbf33c68405d84d6b6ee91e5
danny-may <dannyjmay97@gmail.com>
2021-08-16 12:22:09 +0100

    Add nato command

---
+++ src/workers/cluster/dcommands/general/nato.ts
@@ -0,0 +1,69 @@
+import { BaseGlobalCommand } from '@cluster/command';
+import { CommandType } from '@cluster/utils';
+
+export class NatoCommand extends BaseGlobalCommand {
+    public constructor() {
+        super({
+            name: 'nato',
+            category: CommandType.GENERAL,
+            definitions: [
+                {
+                    parameters: '{text+}',
+                    description: 'Translates the given text into the NATO phonetic alphabet.',
+                    execute: (_, [text]) => this.natoify(text)
+                }
+            ]
+        });
+    }
+
+    public natoify(text: string): string {
+        const result = [];
+        let other = '';
+        for (const char of text) {
+            const lower = char.toLowerCase();
+            if (chars.includes(lower)) {
+                if (other !== '') {
+                    result.push(other);
+                    other = '';
+                }
+                result.push(natoMap[lower]);
+            } else {
+                other += char;
+            }
+        }
+        if (other !== '')
+            result.push(other);
+
+        return result.join(' ');
+    }
+}
+
+const natoMap = {
+    a: 'Alpha',
+    b: 'Bravo',
+    c: 'Charlie',
+    d: 'Delta',
+    e: 'Echo',
+    f: 'Foxtrot',
+    g: 'Golf',
+    h: 'Hotel',
+    i: 'India',
+    j: 'Juliett',
+    k: 'Kilo',
+    l: 'Lima',
+    m: 'Mike',
+    n: 'November',
+    o: 'Oscar',
+    p: 'Papa',
+    q: 'Quebec',
+    r: 'Romeo',
+    s: 'Sierra',
+    t: 'Tango',
+    u: 'Uniform',
+    v: 'Victor',
+    w: 'Whiskey',
+    x: 'Xray',
+    y: 'Yankee',
+    z: 'Zulu'
+} as const;
+const chars = Object.keys(natoMap);
