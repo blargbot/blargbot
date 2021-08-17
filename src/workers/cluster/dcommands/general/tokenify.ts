d9314bda3e2c0e05e00339dbe8622ee23bd3b278
danny-may <dannyjmay97@gmail.com>
2021-08-17 15:38:56 +0100

    Add tokenify command

---
+++ src/workers/cluster/dcommands/general/tokenify.ts
@@ -0,0 +1,40 @@
+import { BaseGlobalCommand } from '@cluster/command';
+import { CommandType, randInt } from '@cluster/utils';
+
+export class TokenifyCommand extends BaseGlobalCommand {
+    public constructor() {
+        super({
+            name: 'tokenify',
+            category: CommandType.GENERAL,
+            definitions: [
+                {
+                    parameters: '{input+}',
+                    description: 'Converts the given input into a token.',
+                    execute: (_, [input]) => this.tokenify(input)
+                }
+            ]
+        });
+    }
+
+    public tokenify(input: string): string {
+        const pasta = input.replace(/[^0-9a-z]/gi, '').toLowerCase();
+        const newPasta = [];
+
+        for (let i = 0; i < pasta.length; i++) {
+            newPasta.push(randInt(1, 4) >= 3
+                ? pasta[i].toUpperCase()
+                : pasta[i].toLowerCase());
+
+            if (i === pasta.length - 1)
+                newPasta.length; // NOOP
+            else if (randInt(1, 20) === 1)
+                newPasta.push('.');
+            else if (randInt(1, 30) === 1)
+                newPasta.push('-');
+            else if (randInt(1, 30) === 30)
+                newPasta.push('\\_');
+        }
+
+        return newPasta.join('');
+    }
+}
