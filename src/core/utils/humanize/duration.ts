c902ced9c2d2aca39cfd7618b72f42e0f7d52f91
Brian <nicelink.brian@gmail.com>
2021-08-18 15:57:28 +0200

    Allow limiting of parts count

--- src/core/utils/humanize/duration.ts
+++ src/core/utils/humanize/duration.ts
@@ -2,10 +2,19 @@ import moment, { Duration, Moment } from 'moment-timezone';
 
 import { smartJoin } from './smartJoin';
 
-export function duration(duration: Duration): string;
-export function duration(start: Moment, end: Moment): string;
-export function duration(...args: [Duration] | [Moment, Moment]): string {
-    const diff = args.length === 1 ? args[0] : moment.duration(args[0].diff(args[1]));
+export function duration(duration: Duration, partsLimit?: number): string;
+export function duration(start: Moment, end: Moment, partsLimit?: number): string;
+export function duration(...args: [Duration, number?] | [Moment, Moment, number?]): string {
+    let diff;
+    let partsLimit: number | undefined;
+    if (!moment.isDuration(args[0]) && moment.isMoment(args[1])) {
+        diff = moment.duration(args[0].diff(args[1]));
+        partsLimit = args[2];
+    } else {
+        diff = args[0];
+        partsLimit = typeof args[1] === 'number' ? args[1] : undefined;
+    }
+
     const parts = [];
     if (diff.days() > 0) parts.push(`${diff.days()} days`);
     if (diff.hours() > 0) parts.push(`${diff.hours()} hours`);
@@ -15,6 +24,7 @@ export function duration(...args: [Duration] | [Moment, Moment]): string {
 
     if (parts.length === 0)
         return '0 seconds';
-
+    if (partsLimit !== undefined)
+        return smartJoin(parts.slice(0, partsLimit), ', ', ' and ');
     return smartJoin(parts, ', ', ' and ');
 }
