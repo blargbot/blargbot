15b7c7dccaa4fc7ce7f1609842476538bb190d56
Brian <nicelink.brian@gmail.com>
2021-08-20 19:32:33 +0200

    Allow calling without arguments for empty json (#279)

--- src/tags/json.js
+++ src/tags/json.js
@@ -12,13 +12,13 @@ const Builder = require('../structures/TagBuilder');
 module.exports =
     Builder.ArrayTag('json')
         .withAlias('j')
-        .withArgs(a => [a.require('input')])
-        .withDesc('Defines a raw JSON object without using subtags.')
+        .withArgs(a => [a.optional('input')])
+        .withDesc('Defines a raw JSON object without using subtags. If `input` is omitted, this will return an empty JSON object.')
         .withExample(
             '{json;{\n  "key": "value"\n}}',
             '{\n  "key": "value"\n}'
         ).resolveArgs(-1)
-        .whenArgs(0, Builder.errors.notEnoughArguments)
+        .whenArgs(0, () => '{}')
         .whenArgs(1, async function (subtag, context, args) {
             let raw = args[0].content;
             try {
