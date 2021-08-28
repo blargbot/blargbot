26f0ab74f241bbb15ea9c0a603ef138393e6368e
Adheesh Trivedi <84070263+AdhTri-001@users.noreply.github.com>
2021-08-28 21:10:34 +0530

    Update Engine.js
    Fixed the way blargbot outputs the timestamp

--- src/structures/bbtag/Engine.js
+++ src/structures/bbtag/Engine.js
@@ -233,7 +233,7 @@ async function runTag(content, context) {
         let diff = Date.now() - cdDate;
         if (diff < 0) {
             let retry_after = Math.floor(cdDate / 1000);
-            await bu.send(context.msg, `This ${context.isCC ? 'custom command' : 'tag'} is currently under cooldown. Please try again in <t:${retry_after}:R> seconds.`);
+            await bu.send(context.msg, `This ${context.isCC ? 'custom command' : 'tag'} is currently under cooldown. Please try again <t:${retry_after}:R>.`);
             return;
         }
     }
