7d09bd5d3c4a233567fae2d30005394ef09f7062
danny-may <dannyjmay97@gmail.com>
2021-12-13 13:00:19 +0000

    fix userUpdate event handler

--- src/workers/cluster/events/discord/userUpdate.ts
+++ src/workers/cluster/events/discord/userUpdate.ts
@@ -1,27 +1,29 @@
 import { Cluster } from '@cluster';
 import { guard } from '@cluster/utils';
 import { DiscordEventService } from '@core/serviceTypes';
-import { User } from 'eris';
+import { PartialUser, User } from 'eris';
 
 export class DiscordUserUpdateHandler extends DiscordEventService<'userUpdate'> {
     public constructor(protected readonly cluster: Cluster) {
-        super(cluster.discord, 'userUpdate', cluster.logger, async (user, oldUser) => {
-            if (user.id === this.cluster.discord.user.id)
-                return;
+        super(cluster.discord, 'userUpdate', cluster.logger, (user, oldUser) => this.execute(user, oldUser));
+    }
 
-            const promises: Array<Promise<unknown>> = [this.cluster.database.users.upsert(user)];
-            if (!guard.hasValue(oldUser)) {
-                await Promise.all(promises);
-                return;
-            }
+    public async execute(user: User | undefined | null, oldUser: PartialUser | null | undefined): Promise<void> {
+        if (!guard.hasValue(user) || user.id === this.cluster.discord.user.id)
+            return;
 
-            if (oldUser.username !== user.username || oldUser.discriminator !== user.discriminator)
-                promises.push(this.cluster.moderation.eventLog.userTagUpdated(user, new User({ ...oldUser }, this.cluster.discord)));
-
-            if (oldUser.avatar !== user.avatar)
-                promises.push(this.cluster.moderation.eventLog.userAvatarUpdated(user, new User({ ...oldUser }, this.cluster.discord)));
-
+        const promises: Array<Promise<unknown>> = [this.cluster.database.users.upsert(user)];
+        if (!guard.hasValue(oldUser)) {
             await Promise.all(promises);
-        });
+            return;
+        }
+
+        if (oldUser.username !== user.username || oldUser.discriminator !== user.discriminator)
+            promises.push(this.cluster.moderation.eventLog.userTagUpdated(user, new User({ ...oldUser }, this.cluster.discord)));
+
+        if (oldUser.avatar !== user.avatar)
+            promises.push(this.cluster.moderation.eventLog.userAvatarUpdated(user, new User({ ...oldUser }, this.cluster.discord)));
+
+        await Promise.all(promises);
     }
 }
