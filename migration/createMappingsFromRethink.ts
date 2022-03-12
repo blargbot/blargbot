7b4578ede5661409d371c3baa2eada1391526307
danny-may <dannyjmay97@gmail.com>
2022-03-12 22:15:41 +0000

    Fix CI build

--- migration/createMappingsFromRethink.ts
+++ migration/createMappingsFromRethink.ts
@@ -17,9 +17,7 @@ void (async function () {
         })
     ]);
 
-    const dbName = config.rethink.db as string | undefined;
-
-    const tables = await r.db(dbName ?? 'blargbot').tableList().run(rethink);
+    const tables = await r.db(config.rethink.db).tableList().run(rethink);
     logger.info('Found tables', tables);
     for (const table of tables) {
         logger.info('Getting data from table', table);
