b241f8ae52301e79f06e51ccfc0ec2aecfd43af6
danny-may <dannyjmay97@gmail.com>
2022-03-21 22:09:15 +0000

    Fix api logging

--- src/api/Api.ts
+++ src/api/Api.ts
@@ -28,7 +28,8 @@ export class Api extends BaseClient {
         this.app = express();
         this.app.use(express.json());
         this.app.use((req, resp, next) => {
-            resp.once('finish', () => this.logger.website(`[${resp.statusCode} ${resp.statusMessage}]`, req.method, req.url));
+            const requestStr = `${req.method} ${req.url}`;
+            resp.once('finish', () => this.logger.website(`[${resp.statusCode} ${resp.statusMessage}]`, requestStr));
             next();
         });
 
