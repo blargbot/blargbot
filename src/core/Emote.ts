917c53e7800651bfebf067ca5ce18fb6b3d961d9
danny-may <dannyjmay97@gmail.com>
2022-03-24 00:33:15 +0000

    Nab emote data from discord to suppliment twemoji

--- src/core/Emote.ts
+++ src/core/Emote.ts
@@ -2,10 +2,47 @@ import { Snowflake } from 'catflake';
 import { PartialEmoji } from 'eris';
 import twemoji from 'twemoji';
 
+import emoteData from './discord-emote-data.json';
 import { snowflake } from './utils';
 
 const guildEmoteRegex = /<(?<animated>a?):(?<name>[\w_]{1,32}):(?<id>\d{17,23})>/g;
 const guildApiEmoteRegex = /(?<name>[\w_]{1,32}):(?<id>\d{17,23})/g;
+const discordEmotes = Object.values(emoteData)
+    .flat()
+    .flatMap(entry => 'diversityChildren' in entry ? [entry, ...entry.diversityChildren ?? []] : [entry])
+    .map(entry => entry.surrogates);
+
+const isLeaf: unique symbol = Symbol();
+interface CodepointTree {
+    [key: string]: CodepointTree;
+}
+
+const codepointTree = {} as CodepointTree;
+for (const emote of discordEmotes) {
+    let node = codepointTree;
+    for (const char of emote.split('')) {
+        const code = char.codePointAt(0) ?? 0;
+        node = node[code.toString(16).padStart(4, '0')] ??= {};
+    }
+    Object.assign(node, { [isLeaf]: true });
+}
+
+function buildRegexStr(tree: CodepointTree): string[] {
+    return Object.entries(tree)
+        .map(([codepoint, children]) => {
+            const innerRegex = buildRegexStr(children);
+            if (innerRegex.length === 0)
+                return `\\u${codepoint}`;
+            if (innerRegex.length === 1)
+                return isLeaf in children
+                    ? `\\u${codepoint}(?:${innerRegex[0]})?`
+                    : `\\u${codepoint}${innerRegex[0]}`;
+            return isLeaf in children
+                ? `\\u${codepoint}(?:${innerRegex.join('|')})?`
+                : `\\u${codepoint}(?:${innerRegex.join('|')})`;
+        }).sort((a, b) => b.length - a.length);
+}
+const discordEmoteRegex = new RegExp(buildRegexStr(codepointTree).join('|'), 'g');
 
 export class Emote {
     public static findAll(this: void, text: string): Emote[] {
@@ -33,9 +70,11 @@ export class Emote {
         text = text.replaceAll(guildEmoteRegex, (_, animated: string, name: string, id: Snowflake) => {
             emotes.push(new Emote(name, id, animated !== ''));
             return '';
-        });
-        text = text.replaceAll(guildApiEmoteRegex, (_, name: string, id: Snowflake) => {
+        }).replaceAll(guildApiEmoteRegex, (_, name: string, id: Snowflake) => {
             emotes.push(new Emote(name, id, false));
+            return '';
+        }).replaceAll(discordEmoteRegex, emote => {
+            emotes.push(new Emote(emote, undefined, false));
             return '';
         });
         twemoji.parse(text, {
@@ -80,3 +119,6 @@ export class Emote {
         return `<:${this.name}:${this.id}>`;
     }
 }
+
+const emotes = Emote.findAll(discordEmotes.join(' '));
+emotes;
