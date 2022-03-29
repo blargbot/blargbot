ce89426429df06921c523dde433b56108d9be09d
danny-may <dannyjmay97@gmail.com>
2022-03-29 15:20:30 +0100

    No point in logging process.exit

--- src/logger/createLogger.ts
+++ src/logger/createLogger.ts
@@ -20,19 +20,7 @@ export function createLogger(config: Configuration, workerId: string): Logger {
     if (config.sentry.base !== '')
         logger.addPreHook(createSentryPreHook(config, workerId) as unknown as ArgHookCallback);
 
-    const result = logger as unknown as Logger;
-
-    const exit = process.exit.bind(process);
-    process.exit = (...args) => {
-        try {
-            result.fatal(new Error(`process.exit(${args.join(', ')}) has been called`));
-        } finally {
-            exit(...args);
-        }
-        throw null;
-    };
-
-    return result;
+    return logger as unknown as Logger;
 }
 
 const logLevels: Record<LogLevel, { color: typeof CatLoggr['_chalk']; isError?: boolean; isTrace?: boolean; }> = {
