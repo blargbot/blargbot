0878a4d825f81b13e70c8cca96ba57c5ad413f0d
danny-may <dannyjmay97@gmail.com>
2022-03-31 19:45:26 +0100

    Cleanup error code

--- src/cluster/events/discord/shardDisconnect.ts
+++ src/cluster/events/discord/shardDisconnect.ts
@@ -4,7 +4,7 @@ import { DiscordEventService } from '@blargbot/core/serviceTypes';
 export class DiscordShardDisconnectHandler extends DiscordEventService<'shardDisconnect'> {
     public constructor(cluster: Cluster) {
         super(cluster.discord, 'shardDisconnect', cluster.logger, (error, shardId) => {
-            this.logger.shardi('shard', shardId, 'has disconnected', error);
+            this.logger.shardi('shard', shardId, 'has disconnected', error?.message);
         });
     }
 }
