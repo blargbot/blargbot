97a347f2eaca0b8470644fdebda76a9ad8226771
Danny May <dannyjmay97@gmail.com>
2022-04-08 09:57:49 +0100

    Add allowed mentions to webhook payload

--- src/tags/webhook.js
+++ src/tags/webhook.js
@@ -22,13 +22,13 @@ module.exports =
             a.optional('filename')
         ])
         .withDesc('Executes a webhook. The `embed` must be provided in a raw JSON format, properly escaped for BBTag. ' +
-        'A simple escaping utility can be accessed [here](https://rewrite.blargbot.xyz/v1escaper). ' +
-        'You can find an easy tool to test out embeds [here](https://leovoel.github.io/embed-visualizer/). ' +
-        'Please assign your webhook credentials to private variables! Do not leave them in your code. ' +
-        'If `file` starts with `buffer:`, the following text will be parsed as base64 to a raw buffer.')
+            'A simple escaping utility can be accessed [here](https://rewrite.blargbot.xyz/v1escaper). ' +
+            'You can find an easy tool to test out embeds [here](https://leovoel.github.io/embed-visualizer/). ' +
+            'Please assign your webhook credentials to private variables! Do not leave them in your code. ' +
+            'If `file` starts with `buffer:`, the following text will be parsed as base64 to a raw buffer.')
         .withExample(
-        '{webhook;1111111111111111;t.OK-en;Hello!}',
-        'In the webhook channel: Hello!'
+            '{webhook;1111111111111111;t.OK-en;Hello!}',
+            'In the webhook channel: Hello!'
         )
         .whenArgs('0-1', Builder.errors.notEnoughArguments)
         .whenArgs('2-8', async function (subtag, context, args) {
@@ -56,7 +56,8 @@ module.exports =
                     avatarURL: avatar,
                     content: content,
                     embeds: embed ? (Array.isArray(embed) ? embed : [embed]) : [],
-                    file
+                    file,
+                    allowedMentions: await context.getAllowedMentions()
                 });
             }
             catch (err) {
