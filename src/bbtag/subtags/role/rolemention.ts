8c746d9048247c3a70697844afe93ff26ef1771f
danny-may <dannyjmay97@gmail.com>
2022-06-09 09:05:27 +0100

    Add noPing to rolemention

--- src/bbtag/subtags/role/rolemention.ts
+++ src/bbtag/subtags/role/rolemention.ts
@@ -1,6 +1,8 @@
+import { parse } from '@blargbot/core/utils';
+
 import { BBTagContext } from '../../BBTagContext';
 import { CompiledSubtag } from '../../compilation';
-import { RoleNotFoundError } from '../../errors';
+import { NotABooleanError, RoleNotFoundError } from '../../errors';
 import { SubtagType } from '../../utils';
 
 export class RoleMentionSubtag extends CompiledSubtag {
@@ -10,12 +12,12 @@ export class RoleMentionSubtag extends CompiledSubtag {
             category: SubtagType.ROLE,
             definition: [
                 {
-                    parameters: ['role', 'quiet?'],
+                    parameters: ['role', 'quiet?', 'noPing?:false'],
                     description: 'Returns a mention of `role`. If `quiet` is specified, if `role` can\'t be found it will simply return nothing.',
                     exampleCode: 'The admin role will be mentioned: {rolemention;Admin}',
                     exampleOut: 'The admin role will be mentioned: @\u200BAdminstrator',
                     returns: 'string',
-                    execute: (ctx, [roleId, quiet]) => this.roleMention(ctx, roleId.value, quiet.value !== '')
+                    execute: (ctx, [roleId, quiet, noPing]) => this.roleMention(ctx, roleId.value, quiet.value !== '', noPing.value)
                 }
             ]
         });
@@ -24,9 +26,14 @@ export class RoleMentionSubtag extends CompiledSubtag {
     public async roleMention(
         context: BBTagContext,
         roleId: string,
-        quiet: boolean
+        quiet: boolean,
+        noPingStr: string
     ): Promise<string> {
         quiet ||= context.scopes.local.quiet ?? false;
+        const noPing = parse.boolean(noPingStr);
+        if (noPing === undefined)
+            throw new NotABooleanError(noPing);
+
         const role = await context.queryRole(roleId, { noLookup: quiet });
 
         if (role === undefined) {
@@ -34,7 +41,7 @@ export class RoleMentionSubtag extends CompiledSubtag {
                 .withDisplay(quiet ? '' : undefined);
         }
 
-        if (!context.data.allowedMentions.roles.includes(role.id))
+        if (!noPing && !context.data.allowedMentions.roles.includes(role.id))
             context.data.allowedMentions.roles.push(role.id);
         return role.mention;
     }
