5659bb525829aaa440419f79e4f3c8116ba172b0
danny-may <dannyjmay97@gmail.com>
2022-06-16 19:05:15 +0100

    Fix jset create regression

--- src/bbtag/subtags/json/jsonset.ts
+++ src/bbtag/subtags/json/jsonset.ts
@@ -15,7 +15,17 @@ export class JsonSetSubtag extends CompiledSubtag {
                     exampleCode: '{set;~json;{json;{"key" : "value"}}}\n{jset;~json;key}\n{get;~json}',
                     exampleOut: '{}',
                     returns: 'json|nothing',
-                    execute: (ctx, [input, path]) => this.setValue(ctx, input.value, path.value, undefined, 'false')
+                    execute: (ctx, [input, path]) => this.setValue(ctx, input.value, path.value, undefined, false)
+                },
+                {
+                    parameters: ['input:{}', 'path', 'value'],
+                    description: 'Using the `input` as a base, navigates the provided dot-notated `path` and assigns the `value`. ' +
+                        '`input` can be a JSON object, array, or string. If a string is provided, a variable with the same name will be used.' +
+                        'If `create` is not empty, will create/convert any missing keys.',
+                    exampleCode: '{jsonset;;path.to.key;value;create}',
+                    exampleOut: '{"path":{"to":{"key":"value"}}}',
+                    returns: 'json|nothing',
+                    execute: (ctx, [input, path, value]) => this.setValue(ctx, input.value, path.value, value.value, false)
                 },
                 {
                     parameters: ['input:{}', 'path', 'value', 'create?'],
@@ -25,7 +35,7 @@ export class JsonSetSubtag extends CompiledSubtag {
                     exampleCode: '{jsonset;;path.to.key;value;create}',
                     exampleOut: '{"path":{"to":{"key":"value"}}}',
                     returns: 'json|nothing',
-                    execute: (ctx, [input, path, value, create]) => this.setValue(ctx, input.value, path.value, value.value, create.value)
+                    execute: (ctx, [input, path, value]) => this.setValue(ctx, input.value, path.value, value.value, true)
                 }
             ]
         });
@@ -36,9 +46,8 @@ export class JsonSetSubtag extends CompiledSubtag {
         input: string,
         path: string,
         value: string | undefined,
-        createStr: string
+        create: boolean
     ): Promise<JToken | undefined> {
-        const create = createStr !== '' ? true : false;
         const target = await bbtag.json.resolveObj(context, input);
 
         bbtag.json.set(target.object, path, value, create);
