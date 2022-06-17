be6028816e4ec9becd83af0938b434264b28b135
danny-may <dannyjmay97@gmail.com>
2022-06-17 16:38:26 +0100

    Update jget to support empty path

--- src/bbtag/subtags/json/jsonget.ts
+++ src/bbtag/subtags/json/jsonget.ts
@@ -10,7 +10,16 @@ export class JsonGetSubtag extends CompiledSubtag {
             aliases: ['jget'],
             definition: [
                 {
-                    parameters: ['input:{}#10000000', 'path?'],
+                    parameters: ['input:{}#10000000'],
+                    description: 'Gets a json value. Works with arrays too!\n' +
+                        '`input` can be a JSON object, array, or string. If a string is provided, a variable with the same name will be used.',
+                    exampleCode: '{jsonget;{j;{\n  "array": [\n    "zero",\n    { "value": "one" },\n    "two"\n  ]\n}};array.1.value}',
+                    exampleOut: 'one',
+                    returns: 'json|nothing',
+                    execute: (ctx, [input]) => this.jsonGet(ctx, input.value, undefined)
+                },
+                {
+                    parameters: ['input:{}#10000000', 'path'],
                     description: 'Navigates the path of a JSON object. Works with arrays too!\n' +
                         '`input` can be a JSON object, array, or string. If a string is provided, a variable with the same name will be used.\n' +
                         '`path` is a dot-noted series of properties.',
@@ -23,8 +32,10 @@ export class JsonGetSubtag extends CompiledSubtag {
         });
     }
 
-    public async jsonGet(context: BBTagContext, input: string, path: string): Promise<JToken | undefined> {
+    public async jsonGet(context: BBTagContext, input: string, path?: string): Promise<JToken | undefined> {
         const obj = (await bbtag.json.resolveObj(context, input)).object;
+        if (path === undefined)
+            return obj;
         return bbtag.json.get(obj, path);
     }
 }
