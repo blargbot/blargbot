148ffff779f4971a69c3469c74622f546817cb9c
Danny May <dannyjmay97@gmail.com>
2022-07-14 23:31:09 +0100

    Centralize guild loading & remove hello message (#361)

--- src/cluster/events/discord/guildDelete.ts
+++ src/cluster/events/discord/guildDelete.ts
@@ -1,13 +1,14 @@
 import { Cluster } from '@blargbot/cluster';
-import { metrics } from '@blargbot/core/Metrics';
 import { DiscordEventService } from '@blargbot/core/serviceTypes';
+import { PossiblyUncachedGuild } from 'eris';
 
 export class DiscordGuildDeleteHandler extends DiscordEventService<'guildDelete'> {
     public constructor(protected readonly cluster: Cluster) {
-        super(cluster.discord, 'guildDelete', cluster.logger, async (guild) => {
-            metrics.guildGauge.dec();
-            await this.cluster.util.postStats();
-            await this.cluster.database.guilds.setActive(guild.id, false);
-        });
+        super(cluster.discord, 'guildDelete', cluster.logger, guild => this.execute(guild));
+    }
+
+    public async execute(guild: PossiblyUncachedGuild): Promise<void> {
+        await this.cluster.guilds.guildLeft(guild.id);
+        await this.cluster.util.postStats();
     }
 }
