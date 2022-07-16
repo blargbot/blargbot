62d2470b8c188367ee53d9d88668bcaf6e7346f3
danny-may <dannyjmay97@gmail.com>
2022-07-16 13:35:53 +0100

    Fix flags in exec(cc)

--- test/bbtag/subtags/bot/exec.test.ts
+++ test/bbtag/subtags/bot/exec.test.ts
@@ -170,6 +170,150 @@ runSubtagTests({
                 expect(ctx.data.stackSize).to.equal(200);
                 expect(ctx.data.state).to.equal(BBTagRuntimeState.ABORT);
             }
+        },
+        {
+            code: '{exec;otherSubtag;arg1;arg2;-f flag value}',
+            expected: 'Success!',
+            subtags: [new AssertSubtag((ctx) => {
+                expect(ctx.parent).to.not.be.undefined;
+                expect(ctx.tagName).to.equal('otherSubtag');
+                expect(ctx.rootTagName).to.equal('test tag');
+                expect(ctx.cooldown).to.equal(7);
+                expect(ctx.inputRaw).to.equal('arg1 arg2 "-f flag value"');
+                expect(ctx.input).to.deep.equal(['arg1', 'arg2', '-f flag value']);
+                expect(ctx.flaggedInput.f?.merge().raw).to.equal('flag value');
+                expect(ctx.flaggedInput._.merge().raw).to.equal('arg1 arg2');
+                expect(ctx.data.stackSize).to.equal(101);
+                expect(ctx.scopes.local).to.not.equal(ctx.scopes.root);
+                expect(ctx.scopes.tag).to.not.equal(ctx.scopes.root);
+                return 'Success!';
+            })],
+            errors: [
+                { start: 8, end: 14, error: new MarkerError('eval', 8) }
+            ],
+            setup(ctx) {
+                ctx.options.cooldown = 4;
+                ctx.options.tagName = 'test tag';
+                ctx.options.rootTagName = 'test tag';
+                ctx.options.inputRaw = 'This is some input text';
+                ctx.options.data = { stackSize: 100 };
+                ctx.tags['otherSubtag'] = {
+                    author: '212097368371683623',
+                    content: '{assert}{eval}',
+                    name: 'otherSubtag',
+                    lastmodified: new Date(),
+                    uses: 0,
+                    cooldown: 7
+                };
+            },
+            assert(ctx) {
+                expect(ctx.parent).to.be.undefined;
+                expect(ctx.tagName).to.equal('test tag');
+                expect(ctx.rootTagName).to.equal('test tag');
+                expect(ctx.cooldown).to.equal(4);
+                expect(ctx.inputRaw).to.equal('This is some input text');
+                expect(ctx.flaggedInput.f?.merge().raw).to.be.undefined;
+                expect(ctx.flaggedInput._.merge().raw).to.equal('This is some input text');
+                expect(ctx.data.stackSize).to.equal(100);
+                expect(ctx.scopes.local).to.equal(ctx.scopes.root);
+                expect(ctx.scopes.tag).to.equal(ctx.scopes.root);
+            }
+        },
+        {
+            code: '{exec;otherSubtag;arg1 arg2 -f flag value}',
+            expected: 'Success!',
+            subtags: [new AssertSubtag((ctx) => {
+                expect(ctx.parent).to.not.be.undefined;
+                expect(ctx.tagName).to.equal('otherSubtag');
+                expect(ctx.rootTagName).to.equal('test tag');
+                expect(ctx.cooldown).to.equal(7);
+                expect(ctx.inputRaw).to.equal('arg1 arg2 -f flag value');
+                expect(ctx.input).to.deep.equal(['arg1', 'arg2', '-f', 'flag', 'value']);
+                expect(ctx.flaggedInput.f?.merge().raw).to.equal('flag value');
+                expect(ctx.flaggedInput._.merge().raw).to.equal('arg1 arg2');
+                expect(ctx.data.stackSize).to.equal(101);
+                expect(ctx.scopes.local).to.not.equal(ctx.scopes.root);
+                expect(ctx.scopes.tag).to.not.equal(ctx.scopes.root);
+                return 'Success!';
+            })],
+            errors: [
+                { start: 8, end: 14, error: new MarkerError('eval', 8) }
+            ],
+            setup(ctx) {
+                ctx.options.cooldown = 4;
+                ctx.options.tagName = 'test tag';
+                ctx.options.rootTagName = 'test tag';
+                ctx.options.inputRaw = 'This is some input text';
+                ctx.options.data = { stackSize: 100 };
+                ctx.tags['otherSubtag'] = {
+                    author: '212097368371683623',
+                    content: '{assert}{eval}',
+                    name: 'otherSubtag',
+                    lastmodified: new Date(),
+                    uses: 0,
+                    cooldown: 7
+                };
+            },
+            assert(ctx) {
+                expect(ctx.parent).to.be.undefined;
+                expect(ctx.tagName).to.equal('test tag');
+                expect(ctx.rootTagName).to.equal('test tag');
+                expect(ctx.cooldown).to.equal(4);
+                expect(ctx.inputRaw).to.equal('This is some input text');
+                expect(ctx.flaggedInput.f?.merge().raw).to.be.undefined;
+                expect(ctx.flaggedInput._.merge().raw).to.equal('This is some input text');
+                expect(ctx.data.stackSize).to.equal(100);
+                expect(ctx.scopes.local).to.equal(ctx.scopes.root);
+                expect(ctx.scopes.tag).to.equal(ctx.scopes.root);
+            }
+        },
+        {
+            code: '{exec;otherSubtag;arg1 arg2 \\-f flag value}',
+            expected: 'Success!',
+            subtags: [new AssertSubtag((ctx) => {
+                expect(ctx.parent).to.not.be.undefined;
+                expect(ctx.tagName).to.equal('otherSubtag');
+                expect(ctx.rootTagName).to.equal('test tag');
+                expect(ctx.cooldown).to.equal(7);
+                expect(ctx.inputRaw).to.equal('arg1 arg2 \\-f flag value');
+                expect(ctx.input).to.deep.equal(['arg1', 'arg2', '-f', 'flag', 'value']);
+                expect(ctx.flaggedInput.f?.merge().raw).to.be.undefined;
+                expect(ctx.flaggedInput._.merge().raw).to.equal('arg1 arg2 \\-f flag value');
+                expect(ctx.data.stackSize).to.equal(101);
+                expect(ctx.scopes.local).to.not.equal(ctx.scopes.root);
+                expect(ctx.scopes.tag).to.not.equal(ctx.scopes.root);
+                return 'Success!';
+            })],
+            errors: [
+                { start: 8, end: 14, error: new MarkerError('eval', 8) }
+            ],
+            setup(ctx) {
+                ctx.options.cooldown = 4;
+                ctx.options.tagName = 'test tag';
+                ctx.options.rootTagName = 'test tag';
+                ctx.options.inputRaw = 'This is some input text';
+                ctx.options.data = { stackSize: 100 };
+                ctx.tags['otherSubtag'] = {
+                    author: '212097368371683623',
+                    content: '{assert}{eval}',
+                    name: 'otherSubtag',
+                    lastmodified: new Date(),
+                    uses: 0,
+                    cooldown: 7
+                };
+            },
+            assert(ctx) {
+                expect(ctx.parent).to.be.undefined;
+                expect(ctx.tagName).to.equal('test tag');
+                expect(ctx.rootTagName).to.equal('test tag');
+                expect(ctx.cooldown).to.equal(4);
+                expect(ctx.inputRaw).to.equal('This is some input text');
+                expect(ctx.flaggedInput.f?.merge().raw).to.be.undefined;
+                expect(ctx.flaggedInput._.merge().raw).to.equal('This is some input text');
+                expect(ctx.data.stackSize).to.equal(100);
+                expect(ctx.scopes.local).to.equal(ctx.scopes.root);
+                expect(ctx.scopes.tag).to.equal(ctx.scopes.root);
+            }
         }
     ]
 });
