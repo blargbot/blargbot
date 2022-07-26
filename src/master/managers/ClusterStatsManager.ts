439f58116565ea9cb24a15a8f83ef57e5675ac42
Danny May <dannyjmay97@gmail.com>
2022-07-26 15:00:08 +0100

    Fix incorrect message from unresponsive cluster & reduce double respawn risk (#367)

--- src/master/managers/ClusterStatsManager.ts
+++ src/master/managers/ClusterStatsManager.ts
@@ -1,27 +1,38 @@
 import { ApiConnection } from '@blargbot/api';
+import { ClusterConnection } from '@blargbot/cluster';
 import { ClusterStats } from '@blargbot/cluster/types';
 import { WorkerPool } from '@blargbot/core/worker';
 
 export class ClusterStatsManager {
-    private readonly statsMap: Record<number, ClusterStats | undefined>;
+    readonly #statsMap: WeakMap<ClusterConnection, ClusterStats>;
+    readonly #activeClusters: Map<number, ClusterConnection>;
 
     public constructor(private readonly apis: WorkerPool<ApiConnection>) {
-        this.statsMap = {};
+        this.#statsMap = new Map();
+        this.#activeClusters = new Map();
     }
 
-    public set(clusterId: number, stats: ClusterStats | undefined): void {
+    public set(cluster: ClusterConnection, stats: ClusterStats | undefined): void {
         if (stats === undefined)
-            delete this.statsMap[clusterId];
+            this.#statsMap.delete(cluster);
         else
-            this.statsMap[clusterId] = stats;
-        this.apis.forEach((_, api) => api?.send('clusterStats', this.statsMap));
+            this.#statsMap.set(cluster, stats);
+
+        const current = this.#activeClusters.get(cluster.id);
+        if (current?.created.isAfter(cluster.created) === true)
+            return;
+
+        this.#activeClusters.set(cluster.id, cluster);
+        this.apis.forEach((_, api) => api?.send('clusterStats', this.getAll()));
     }
 
-    public get(clusterId: number): ClusterStats | undefined {
-        return this.statsMap[clusterId];
+    public get(cluster: ClusterConnection): ClusterStats | undefined {
+        return this.#statsMap.get(cluster);
     }
 
     public getAll(): Record<number, ClusterStats | undefined> {
-        return this.statsMap;
+        return Object.fromEntries(
+            [...this.#activeClusters.values()].map(cluster => [cluster.id, this.get(cluster)] as const)
+        );
     }
 }
