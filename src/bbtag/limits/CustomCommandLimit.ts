2df5663391c282b5788b268eee6867e84278f13b
danny-may <dannyjmay97@gmail.com>
2022-07-29 15:15:20 +0100

    Fix timeout modlog and restrictions

--- src/bbtag/limits/CustomCommandLimit.ts
+++ src/bbtag/limits/CustomCommandLimit.ts
@@ -14,6 +14,7 @@ export class CustomCommandLimit extends GlobalLimit {
             .addRules('unban', staffOnlyRule)
             .addRules('guildbans', staffOnlyRule)
             .addRules('kick', staffOnlyRule)
+            .addRules('timeout', staffOnlyRule)
             .addRules('modlog', staffOnlyRule)
             .addRules('pardon', staffOnlyRule)
             .addRules('warn', staffOnlyRule)
--- src/bbtag/limits/GlobalLimit.ts
+++ src/bbtag/limits/GlobalLimit.ts
@@ -14,6 +14,7 @@ export abstract class GlobalLimit extends BaseRuntimeLimit {
                 'edit',
                 'delete',
                 'kick',
+                'timeout',
                 'ban',
                 'reactadd',
                 'reactremove',
