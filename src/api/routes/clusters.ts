8510d37f9afbc89ea030c04a1be621fe8b2f0085
danny-may <dannyjmay97@gmail.com>
2022-09-02 21:51:49 +0100

    Revert some functional changes

--- src/api/routes/clusters.ts
+++ src/api/routes/clusters.ts
@@ -18,7 +18,7 @@ export class ClustersRoute extends BaseRoute {
 
         this.#api.worker.on('clusterStats', ({ data }) => {
             this.#clusterStats = data;
-            api.logger.verbose('Sending cluster stats to', this.#sockets.size, 'connected clients');
+            this.#api.logger.verbose('Sending cluster stats to', this.#sockets.size, 'connected clients');
             for (const socket of this.#sockets)
                 socket.send(JSON.stringify(data));
         });
--- src/api/routes/metrics.ts
+++ src/api/routes/metrics.ts
@@ -2,7 +2,6 @@ import { metrics } from '@blargbot/core/Metrics';
 
 import { Api } from '../Api';
 import { BaseRoute } from '../BaseRoute';
-import { ApiResponse } from '../types';
 
 export class MetricsRoute extends BaseRoute {
     readonly #api: Api;
@@ -13,15 +12,13 @@ export class MetricsRoute extends BaseRoute {
         this.#api = api;
 
         this.addRoute('/', {
-            get: () => this.getMetrics()
+            get: async () => {
+                const retrievedMetrics = await this.#api.worker.request('getMetrics', undefined);
+                metrics.registryCache = Object.values(retrievedMetrics);
+                const register = await metrics.getAggregated();
+
+                return this.ok(await register.metrics(), register.contentType);
+            }
         });
-    }
-
-    public async getMetrics(): Promise<ApiResponse> {
-        const retrievedMetrics = await this.#api.worker.request('getMetrics', undefined);
-        metrics.registryCache = Object.values(retrievedMetrics);
-        const register = await metrics.getAggregated();
-
-        return this.ok(await register.metrics(), register.contentType);
     }
 }
