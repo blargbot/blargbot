a7031e56b094a6de03bdb4fc38b082d5616a477d
Danny May <dannyjmay97@gmail.com>
2022-10-08 17:35:46 +0100

    Add domains API route (#409)

---
+++ src/api/routes/domains.ts
@@ -0,0 +1,20 @@
+import { Api } from '../Api';
+import { BaseRoute } from '../BaseRoute';
+
+export class MetricsRoute extends BaseRoute {
+    readonly #api: Api;
+
+    public constructor(api: Api) {
+        super('/domains');
+
+        this.#api = api;
+
+        this.addRoute('/', {
+            get: async () => {
+                const domains = await this.#api.database.vars.get('whitelistedDomains');
+                const allowed = Object.entries(domains?.values ?? {}).filter(x => x[1]).map(x => x[0]);
+                return this.ok(allowed);
+            }
+        });
+    }
+}
