38e402d8291af779d828efe4dc2cd3d11cb80ba8
Danny May <dannyjmay97@gmail.com>
2022-12-21 23:29:19 +0000

    Update index generator file

--- components/bbtag-core/src/subtags/index.template.ts
+++ components/bbtag-core/src/subtags/index.template.ts
@@ -2,22 +2,17 @@ import fs from 'node:fs/promises';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 
-global.console.log('Creating index file');
 const dir = path.dirname(fileURLToPath(import.meta.url));
 const files = [] as string[];
-for (const fsi of await fs.readdir(dir, { withFileTypes: true })) {
-    if (!fsi.isDirectory())
-        continue;
-
-    for (const ifsi of await fs.readdir(path.join(dir, fsi.name), { withFileTypes: true })) {
-        if (!ifsi.isFile() || ifsi.name.endsWith('index.ts') || !ifsi.name.endsWith('.ts'))
-            continue;
-
-        files.push(path.join(dir, fsi.name, `${ifsi.name.slice(0, -3)}.js`));
-    }
-}
-
-const exports = files.map(f => `export * from './${path.relative(dir, f)}';`).join('\n');
+global.console.log('Finding subtag files');
+for (const groupDir of await fs.readdir(dir, { withFileTypes: true }))
+    if (groupDir.isDirectory())
+        for (const file of await fs.readdir(path.join(dir, groupDir.name), { withFileTypes: true }))
+            if (file.isFile() && file.name.endsWith('.ts'))
+                files.push(path.join(dir, groupDir.name, `${file.name.slice(0, -3)}.js`));
+global.console.log(`Found ${files.length} subtag files`);
 
 global.console.log('Creating index file');
+const exports = files.map(f => `export * from './${path.relative(dir, f)}';`).join('\n');
 await fs.writeFile(path.join(dir, 'index.ts'), `${exports}\n`);
+global.console.log('Created index file');
